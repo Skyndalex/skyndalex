801b6f9876cc1ba4ccd4a7c973fa776a1e24f447
CZUBIX <47487634+CZUBIX@users.noreply.github.com>
2021-02-25 19:22:27 +0100

    Update tools.js

--- tools.js
+++ tools.js
@@ -160,6 +160,16 @@ exports.load = (gateway, discord) => {
         })
     })
 
+    gateway.command("tools", "say", "Wysyła tekst", "say (tekst)", ["powiedz"], (client, msg) => {
+        if (!client.args[0]) return client.events.error(client, "noargs", msg);
+
+        discord.createMessage(msg, {
+            embed: {
+                description: client.args.join(" "),
+                color: 0x2ecc71
+            }
+        })
+    })
 
     gateway.command("tools", "suggest", "Zaproponuj coś", "suggest (treść propozycji)", ["sugestia", "propozycja"], (client, msg) => {
         if (!client.args[0]) return client.events.error(client, "noargs", msg);
@@ -186,7 +196,7 @@ exports.load = (gateway, discord) => {
     })
 
     gateway.command("tools", "complaint", "Wysyła skargę na użytkownika", "complaint (osoba) (powód)", ["report"], (client, msg) => {
-        if (!client.args[1]) return client.events.error(client, "noargs", msg);
+        if (!(msg.mentions[0] || client.args[1])) return client.events.error(client, "noargs", msg);
 
         discord.createMessage(msg, {
             embed: {
