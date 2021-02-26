316f69bd5c9edb5d6d02a9ea2411cde31ce5774b
MrBoombastic <18630357+MrBoombastic@users.noreply.github.com>
2021-02-26 17:28:48 +0100

    weri gud zmiana
    neil patelka approves

--- dev.js
+++ dev.js
@@ -1,6 +1,6 @@
 exports.load = (gateway, discord) => {
     gateway.command("dev", "eval", "eval", "eval (kod)", [], (client, msg) => {
-        const owners = ["509014773006991376", "636096693712060416"];
+        const owners = ["509014773006991376", "636096693712060416", "363365720295473154"];
         if (!owners.includes(msg.author.id)) return client.events.error(client, "nopermission", msg);
 
         var result;
