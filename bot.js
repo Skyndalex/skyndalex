1f65cbc8c6cd5a5e1dcfbbacdff6e532fe473595
Korrumz2 <55253305+Korrumz2@users.noreply.github.com>
2021-03-04 18:28:58 +0100

    expressjs

--- bot.js
+++ bot.js
@@ -39,11 +39,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('ok')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`http://localhost:${port}`)
 })
 
 gateway.event("ready", (client) => {
