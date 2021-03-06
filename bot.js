64392d3b087d5099d2daa07f0fddaaec1a97d105
Korrumz2 <55253305+Korrumz2@users.noreply.github.com>
2021-03-06 18:58:47 +0100

    express

--- bot.js
+++ bot.js
@@ -33,6 +33,17 @@ gateway.event("MESSAGE_CREATE", (client, msg) => {
             })
         }
     })
+    const express = require('express')
+    const app = express()
+    const port = 3000
+
+    app.get('/', (req, res) => {
+        res.send('OK')
+    })
+
+    app.listen(port, () => {
+        console.log(`http://localhost:${port}`)
+    })
 })
 
 gateway.run()
