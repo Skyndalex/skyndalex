c3ba01ac298c31d984afef883510723fba0a02af
Korrumz2 <55253305+Korrumz2@users.noreply.github.com>
2021-03-07 18:42:04 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const gateway = require("./lib/gateway.js");
 const discord = require("./lib/discord.js");
 const { prefix } = require("./config.json");
+const r = require("rethinkdb");
 
 gateway.registerModules(gateway, discord, [
     "handler",
@@ -11,6 +12,37 @@ gateway.registerModules(gateway, discord, [
     "about",
     "mod"
 ])
+
+gateway.event("ready", (client) => {
+    r.connect({host: "localhost", port: 28015}, (err, con) => {
+        if (err) console.log(err);
+        client.con = con;
+    })
+
+    console.log("Successfully logged in!");
+
+    setInterval(() => {
+        const statuses = [
+            "Prefix: s/",
+            "@Skyndalex",
+        ]
+
+        const data = {
+            op: 3,
+            d: {
+                since: null,
+                activities: [{
+                    name: statuses[Math.floor(Math.random() * statuses.length)],
+                    type: 1
+                }],
+                status: "online",
+                afk: false
+            }
+        }
+
+        client.ws.send(JSON.stringify(data));
+    }, 10000)
+})
 
 gateway.event("MESSAGE_CREATE", (client, msg) => {
     discord.getCurrentUser().then(bot => {
@@ -40,15 +72,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('OK!')
+    res.send('status.skyndalex.tk')
 })
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`)
 })
-
-gateway.event("ready", (client) => {
-    console.log("Successfully logged in!");
-})
-
-gateway.run()
+app.listen();
+gateway.run();
