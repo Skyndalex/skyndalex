7bf7f4b7f7fbd877e41424b650578e44ace5c119
Korrumz2 <55253305+Korrumz2@users.noreply.github.com>
2021-03-17 11:07:01 +0100

    h

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const gateway = require("./lib/gateway.js");
 const discord = require("./lib/discord.js");
 const { prefix } = require("./config.json");
+const { defaultPrefix } = require("./config.json")
 const r = require("rethinkdb");
 const fetch = require('node-fetch')
 gateway.registerModules(gateway, discord, [
@@ -52,28 +53,44 @@ gateway.event("ready", (client) => {
     }, 10000)
 })
 
-gateway.event("MESSAGE_CREATE", (client, msg) => {
-    discord.getCurrentUser().then(bot => {
-        const prefixMention = new RegExp(`^<@!?${bot.id}>( |)$`);
-
-        if (msg.content.match(prefixMention)) {
-            discord.createMessage(msg, {
-                embed: {
-                    title: "Oznaczyłeś mnie!",
-                    description: "Komenda pomocy: ?help",
-                    fields: [
-                        {
-                            name: "Prefix",
-                            value: prefix,
-                            inline: false
+    gateway.event("MESSAGE_CREATE", (client, msg) => {
+        discord.getCurrentUser().then(bot => {
+            async function f1() {
+                const prefixMention = new RegExp(`^<@!?${bot.id}>( |)$`);
+                if (msg.content.match(prefixMention)) {
+                    const res = await r.db("guilds").table('prefix').get(client.guilds.find(x => x.id == "id").prefix).run(client.con)
+                    const prefix = res.prefix ? res.prefix : "Brak";
+                    discord.createMessage(msg, {
+                        embed: {
+                            title: "Oznaczyłeś mnie! Jak się cieszę, że się mną zainteresowałeś",
+                            description: "Komenda pomocy: ?help",
+                            fields: [
+                                {
+                                    name: "Prefix",
+                                    value: prefix,
+                                },
+                                {
+                                  name: 'Prefix defaultowy',
+                                  value: defaultPrefix
+                                },
+                                {
+                                    name: 'Serwer discord bota',
+                                    value: '[Kliknij](https://krivebot.tk/discord)'
+                                },
+                                {
+                                    name: 'Dodaj bota',
+                                    value: '[Kliknij](https://krivebot.tk/invite)'
+                                }
+                            ],
+                            color: 0x2ecc71
                         }
-                    ],
-                    color: 0x2ecc71
+                    })
+
                 }
-            })
-        }
+            }
+            f1()
+        })
     })
-})
 
 
 const app = require("express")();
