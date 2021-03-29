162bd28b860156bedcad67bf70e101ec49efe83e
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-03-29 22:06:22 +0200

    Express server

--- server.js
+++ server.js
@@ -10,7 +10,17 @@ const client = new KriveManager({
 	cacheEmojis: true,
 	cachePresences: false,
 })
+const express = require('express')
+const app = express()
+const port = 6969
 
+app.get('/', (req, res) => {
+	res.send('Sended.')
+})
+
+app.listen(port, () => {
+	console.log(`http://localhost:${port}`)
+})
 const r = require("rethinkdb")
 
 	r.connect({db: "krivebot"}, (err, con) => {
