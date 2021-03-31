0959f40740a4eb5476dc3f23edb04f77a5ce79a3
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-03-31 22:47:18 +0200

    iq

---
+++ commands/fun/iq.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js-light")
+exports.run = async (client, message, args) => {
+    let iq = Math.floor(Math.random() * (100 - 0) + 0)
+    if (!args[0]) return client.error(message, `Nie podano argumentu!`)
+    let embed = new Discord.MessageEmbed()
+        .setDescription(`${args[0]} posiada ${iq} iq!`)
+        .setColor("GREEN")
+    message.channel.send(embed)
+}
+exports.help = {
+    name: "iq",
+    description: "Sprawdza, ile ktoś ma iq",
+    category: "fun",
+}
