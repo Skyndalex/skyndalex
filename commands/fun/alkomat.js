c2db3615ff8e27bd5300d71e3b408ac28093b61a
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-03-31 22:47:02 +0200

    alkomat

---
+++ commands/fun/alkomat.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js-light")
+exports.run = async (client, message, args) => {
+    let v = Math.floor(Math.random() * (10 - 0) + 0)
+    if (!args[0]) return client.error(message, `Nie podano argumentu!`)
+    let embed = new Discord.MessageEmbed()
+        .setDescription(`${args[0]} posiada ${v} promili!`)
+        .setColor("GREEN")
+    message.channel.send(embed)
+}
+exports.help = {
+    name: "alkomat",
+    description: "Sprawdza poziom promili",
+    category: "fun",
+}
