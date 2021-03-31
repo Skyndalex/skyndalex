2dc5fe5e56ca1c5466ddbc944fa987dec6781ba0
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-03-31 21:10:56 +0200

    update

--- commands/bot/social-media.js
+++ commands/bot/social-media.js
@@ -3,7 +3,6 @@ exports.run = async (client, message) => {
 let embed = new Discord.MessageEmbed()
     .setTitle("Social-media bota")
     .setDescription("Bardzo dużo osób się podszywa pod bota, właściciela... W tej komendzie znajdują się linki do oryginalnych, naszych kont")
-    .addField(`Strona internetowa", "[krivebot.xyz](${client.url})`)
     .addField("YouTube", "[Link](https://www.youtube.com/channel/UC_N99xLClLlM9t-DleWwrog)")
     .addField("Twitter", "[Link](https://twitter.com/Krive7)")
     .addField("Właściciel bota", "[Otwórz DM](https://discord.com/users/509014773006991376)")
