dd5479dbdfd8d599c89b4481abe3e515207707b9
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-03-31 22:47:08 +0200

    impostor

---
+++ commands/fun/impostor.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (client, message, args) => {
+    if (!args[0]) return client.error(message, `Nie podano użytkownika!`)
+    let user = client.users.cache.get(args[0])||message.mentions.users.first()||message.author()
+    if (!user) return client.error(message, `Nie znaleziono użytkownika`)
+    let imp = ["jest impostorem", "nie jest impostorem"]
+    let embed = new Discord.MessageEmbed()
+        .setDescription(`<@${user.id}> ${imp.random()}`)
+        .setColor(`GREEN`)
+    message.channel.send(embed)
+}
+exports.help = {
+    name: "impostor",
+    description: "Sprawdza, czy ktoś jest impostorem",
+    category: "fun",
+}
