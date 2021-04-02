2252a43a4e714101567b6c0685d60e1e20127592
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-04-02 23:21:02 +0200

    poprawki

--- Client.js
+++ Client.js
@@ -1,5 +1,6 @@
 const { Client } = require("discord.js-light")
 const Discord = require("discord.js-light")
+
 class KriveManager extends Client {
     constructor(clientOptions) {
         super(clientOptions);
@@ -8,17 +9,7 @@ class KriveManager extends Client {
         this.url = "https://krivebot.xyz"
         this.discord = "https://discord.gg/m6N8mnCBWv"
         this.footer = `KriveBot ${this.version} || ${this.discord}`
-    }
-    //TODO: fields
-    async embed(message, title, text, footer = this.footer, color = "GREEN") {
-        let embed = new Discord.MessageEmbed()
-            .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
-            .setTitle(title)
-            .setDescription(text)
-            .setColor(color)
-            .setFooter(this.footer)
 
-        return message.channel.send(embed)
     }
 }
 module.exports = KriveManager;
--- func.js
+++ func.js
@@ -1,18 +1,15 @@
 const Discord = require("discord.js")
 module.exports = (client) => {
-    client.error = async (message, text) => {
+    client.error = (message, text) => {
         let embedError = new Discord.MessageEmbed()
             .setAuthor(message.author.tag, message.author.displayAvatarURL({dynamic: true}))
             .setTitle(`Wystąpił błąd!`)
             .setDescription(text.replace(client.token, '[token]'))
             .setColor('#fc8700')
-            .setFooter(`Błąd - KriveBot v3 || https://krivebot.tk/discord`, client.user.displayAvatarURL())
-        return message.channel.send(embedError).catch(err => {
-            client.guilds.cache.get("707506586184319036").channels.cache.get("723973152660520980").send(`Problem z \`\`\`${err}\`\`\``)
-            message.author.send(`Wystąpił błąd podczas wykonywania kodu! Poinformowałem programistów.\nBłąd: \`\`\`${err}\`\`\``)
-        })
+            .setFooter(`Błąd - KriveBot v3 || https://${client.url}/discord`, client.user.displayAvatarURL())
+        return message.channel.send(embedError)
     }
-    client.commandNotEnabled = async (message, text) => {
+    client.commandNotEnabled = (message, text) => {
         let embed = new Discord.MessageEmbed()
             .setTitle("Komenda wyłączona!")
             .setDescription(`Z powodu: ${text.replace(client.token, '[token]')}`)
