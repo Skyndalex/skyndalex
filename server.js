752e072ad892b41a8384070e9b8663f1bf679dda
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-04-03 12:49:23 +0200

    Update server.js

--- server.js
+++ server.js
@@ -21,78 +21,14 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
 	console.log(`http://localhost:${port}`)
 })
+
 const r = require("rethinkdb")
-	r.connect({db: "krivebot"}, (err, con) => {
-		if (err) console.log(err)
-		client.con = con;
-	})
+r.connect({db: "krivebot"}, (con) => {
+	client.con = con;
+})
+
 client.on("ready", () => {
 	console.log("Client ready!")
-});
-client.ws.on('INTERACTION_CREATE',  interaction => {
-	switch(interaction.data.name.toLowerCase()) {
-		case 'ping':
-			client.api.interactions(interaction.id, interaction.token).callback.post({
-				data: {
-					type: 4,
-					data: {
-						content: client.ws.ping
-					}
-				}
-			})
-			break;
-		case 'authors':
-			client.api.interactions(interaction.id, interaction.token).callback.post({
-				data: {
-					type: 4,
-					data: {
-						content: "entity#8309"
-					}
-				}
-			})
-			break;
-		case 'discord':
-			client.api.interactions(interaction.id, interaction.token).callback.post({
-				data: {
-					type: 4,
-					data: {
-						content: "https://krivebot.xyz/discord"
-					}
-				}
-			})
-			break;
-		case 'page':
-			client.api.interactions(interaction.id, interaction.token).callback.post({
-				data: {
-					type: 4,
-					data: {
-						content: "https://krivebot.xyz"
-					}
-				}
-			})
-			break;
-		case 'invite':
-			client.api.interactions(interaction.id, interaction.token).callback.post({
-				data: {
-					type: 4,
-					data: {
-						content: "https://krivebot.xyz/invite"
-					}
-				}
-			})
-			break;
-		case 'public':
-			client.api.interactions(interaction.id, interaction.token).callback.post({
-				data: {
-					type: 4,
-					data: {
-						content: "https://krivebot.xyz/public"
-					}
-				}
-			})
-			break;
-	}
-
 });
 
 client.commands = new Discord.Collection();
