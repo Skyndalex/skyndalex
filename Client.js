17b7148466fca4cbbc1cf1f1a5cb252b5ac540a1
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-04-13 21:07:06 +0200

    Update Client.js

--- Client.js
+++ Client.js
@@ -1,15 +1,23 @@
-const { Client } = require("discord.js-light")
-const Discord = require("discord.js-light")
+const { Client } = require("discord.js")
+const Discord = require("discord.js")
 
 class KriveManager extends Client {
     constructor(clientOptions) {
         super(clientOptions);
 
-        this.version = "v2.0"
+        this.version = "v3.0"
         this.url = "https://krivebot.xyz"
         this.discord = "https://discord.gg/m6N8mnCBWv"
         this.footer = `KriveBot ${this.version} || ${this.discord}`
+        this.botname = `KriveBot`
+        this.latestupdate = `USERS & ECONOMY UPDATE`
 
+        this.docsLink = "docs.krivebot.xyz"
+        this.channelConfigLinkDocs = "docs.krivebot.xyz/pl/channel-config"
+        this.autoRoleConfigLinkDocs = "docs.krivebot.xyz/pl/autorole"
+        this.verificationConfigLinkDocs = "docs.krivebot.xyz/pl/verification"
+        this.moneyCollectingLinkDocs = "docs.krivebot.xyz/pl/acquisition-money"
+        this.chancesLinkDocs = "docs.krivebot.xyz/pl/chances"
     }
      error(message, text, footer = "Błąd", color = "RED") {
         let embedError = new Discord.MessageEmbed()
