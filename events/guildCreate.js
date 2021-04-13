c124df038e6a3e7cdc698f50af85f2268fe2df19
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-04-13 21:08:14 +0200

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,6 +1,12 @@
 const r = require("rethinkdb")
 module.exports = async(client, guild) => {
+
    await r.table("settings").insert({
         guildID: guild.id
     }).run(client.con)
+
+   await r.table("moderation").insert({
+       guildID: guild.id
+   }).run(client.con)
+
 }
