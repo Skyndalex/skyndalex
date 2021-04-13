4f9438c9daba3b6722349e9d8e2e2a00a0df5725
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-04-13 21:16:19 +0200

    Update replace.js

--- commands/tools/replace.js
+++ commands/tools/replace.js
@@ -1,4 +1,5 @@
-const Discord = require("discord.js-light")
+const Discord = require("discord.js")
+
 exports.run = async (client, message, args) => {
      if (!message.member.hasPermission('MANAGE_CHANNELS')) return client.error(message, `Nie masz permisji! `);
 
