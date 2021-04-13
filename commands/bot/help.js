92f209712412b653b93f5c222546483e7aea2a2c
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-04-13 21:08:30 +0200

    Update help.js

--- commands/bot/help.js
+++ commands/bot/help.js
@@ -1,5 +1,6 @@
+const Discord = require('discord.js')
+
 exports.run = async (client, message, args) => {
-    const Discord = require('discord.js-light')
     let commandsf = [
         "set",
         "8ball",
