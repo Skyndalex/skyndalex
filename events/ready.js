88c12abb657570c52a348a7e4297e75b89f75626
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-04-13 21:15:18 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
 module.exports = (client) => {
-    let statuses = [`@Krive`, `${client.url}/discord`, `${client.url}/public`, `${client.url}/donate`, `${client.url}/invite`]
-    setInterval(() => client.user.setActivity(statuses.random(), {type: "COMPETING"}), 10000)
+    let statuses = [`@${client.botname}`, `${client.botname} ${client.latestupdate}`, `${client.url}/discord`, `${client.url}/public`, `${client.url}/donate`, `${client.url}/invite`]
+    setInterval(() => client.user.setActivity(statuses.random(), {type: "LISTENING"}), 10000)
 }
