73a1c61edd42953c8ee6eec0872c6a02d8c1cb5f
Korrumz2PL <55253305+Korrumz2PL@users.noreply.github.com>
2021-04-25 19:26:35 +0200

    New unmute-all.js

---
+++ commands/vc management/unmute-all.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js")
+exports.run = async (client, message, args) => {
+    if(!message.member.hasPermission('MANAGE_CHANNELS')) return client.error(message, 'Nie masz permisji!')
+
+    const channel = message.channel;
+    const members = channel.members
+
+    members.forEach(member => {
+        member.voice.setMute(false)
+        member.voice.setDeaf(false)
+    });
+
+    const embed = new Discord.MessageEmbed()
+        .setDescription("Wyłączono wyciszenie na **wszystkich** kanałach głosowych.")
+        .setColor("GREEN")
+    message.channel.send( embed)
+}
+    exports.help = {
+    name: "unmute-all",
+    description: "Odcisz wszystkich użytkowników na kanale głosowym",
+    category: "vcmanagement",
+}
