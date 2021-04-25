ed07569de1fc5e3b2df20c46c8df1de1eba4ce53
Korrumz2PL <55253305+Korrumz2PL@users.noreply.github.com>
2021-04-25 19:27:40 +0200

    Update vckick.js

--- commands/vc management/vckick.js
+++ commands/vc management/vckick.js
@@ -1,14 +1,13 @@
 const Discord = require("discord.js")
 exports.run = async (client, message, args) => {
+    /*
     if (!args[0]) return client.error(message, "Nie podano argumentów!")
 
     const user = message.mentions.users.first().id||client.users.cache.get(args[0]).id
 
     if (!user) return client.error(message, "Nie znaleziono użytkownika.")
 
-    const voiceChannel = message.member.voice.channel;
-    if (!voiceChannel) return client.error(message, "Nie ma użytkownika na kanale głosowym!")
-
+   await ()
 
     const embed = new Discord.MessageEmbed()
         .setTitle("Wyrzucono użytkownika z kanału głosowego!")
@@ -16,6 +15,9 @@ exports.run = async (client, message, args) => {
         .addField("Wyrzucono", user.tag)
         .setColor("GREEN")
     message.channel.send(embed)
+
+     */
+    client.commandNotEnabled(message, "Testy")
 }
 exports.help = {
     name: "vckick",
