223672a5e052f820e1ee6a493153b31eddc1124d
Korrumz2PL <55253305+Korrumz2PL@users.noreply.github.com>
2021-04-25 19:24:44 +0200

    Update mute.js

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -3,7 +3,7 @@ const r = require("rethinkdb")
 exports.run = async (client, message, args) => {
     if (!args[0]) return client.error(message, "Nie podałeś użytkownika")
 
-    const role = await r.table("settings").get(message.guild.id).run(client.con)
+    const role = await r.table("settings").get(message.guild.id)("mutedRole").run(client.con)
     if (!role) return client.error(message, "Nie ustawiono roli!")
 
     const member = message.mentions.members.first() || message.guild.members.cache.get(args[0])
@@ -13,10 +13,10 @@ exports.run = async (client, message, args) => {
     if (!user) return client.error(message, "Nie znaleziono użytkownika")
 
     if (member.id === message.author.id) return client.error(message, 'Na pewno chcesz uciszyć samego siebie? xD....')
-    if (member.id === message.guild.owner.id) return client.error(message, 'Nie możesz uciszyć właściciela serwera')
+    if (member.id === message.guild.ownerID) return client.error(message, 'Nie możesz uciszyć właściciela serwera')
     if (member.roles.highest.rawPosition >= message.member.roles.highest.rawPosition) return client.error(message, 'Nie możesz uciszyć użytkownika z taką samą lub wyższą rolą')
 
-    member.roles.add(role.mutedRole)
+    await member.roles.add(role)
 
     const embed = new Discord.MessageEmbed()
         .setTitle("Uciszono użytkownika!")
