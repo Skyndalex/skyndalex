bf6c4b060b7ff84df326f1f0664ca32392800da9
Korrumz2PL <55253305+Korrumz2PL@users.noreply.github.com>
2021-04-27 11:02:31 +0200

    Update slowmode.js

--- commands/tools/slowmode.js
+++ commands/tools/slowmode.js
@@ -4,13 +4,14 @@ exports.run = async (client, message, args) => {
 
     if (!args[0]) return client.error(message, "Nie podałeś czasu")
     if (isNaN(args[0])) return client.error(message, 'To nie jest liczba!');
+    if ((args[0] > 21600) || (args[0] < -0)) return client.error(message, 'Dłuższy czas cooldownu nie powinien przekraczać 6h.');
 
-        message.channel.setRateLimitPerUser(args[0])
+    await message.channel.setRateLimitPerUser(args[0])
 
     const embed = new Discord.MessageEmbed()
         .setTitle("Ustawiono czas powolny")
         .addField("Ustawił", message.author.tag)
-        .addField("Ustawiono na", `${args[0]}sek`)
+        .addField("Ustawiono na", `${args[0]} sek`)
         .setColor("GREEN")
     message.channel.send(embed)
 }
