228e4acf575554dc6afc4dc1d6b1108c15f27712
Korrumz2PL <55253305+Korrumz2PL@users.noreply.github.com>
2021-04-27 11:02:48 +0200

    Update social-media.js

--- commands/bot/social-media.js
+++ commands/bot/social-media.js
@@ -1,13 +1,13 @@
 const Discord = require("discord.js")
 
-exports.run = async (client, message) => {
+exports.run = (client, message) => {
 let embed = new Discord.MessageEmbed()
-    .setTitle("Social-media bota")
+    .setTitle("Social-media")
     .setDescription("Bardzo dużo osób się podszywa pod bota, właściciela... W tej komendzie znajdują się linki do oryginalnych, naszych kont")
-    .addField("YouTube", "[Link](https://www.youtube.com/channel/UC_N99xLClLlM9t-DleWwrog)")
-    .addField("Twitter", "[Link](https://twitter.com/Krive7)")
-    .addField("Właściciel bota", "[Otwórz DM](https://discord.com/users/509014773006991376)")
-    .addField("Dokumentacja", "[Zobacz](https://docs.krivebot.xyz)")
+    .addField("YouTube", `[Link](${client.youtube})`)
+    .addField("Twitter", `[Link](${client.twitter})`)
+    .addField("Właściciel bota", `[Otwórz DM](${client.dmEntity})`)
+    .addField("Dokumentacja", `[Zobacz](${client.docsLink})`)
     .setFooter("Zachęcamy do zasubskrybowania nas na social-mediach!")
     .setColor("GREEN")
     message.channel.send(embed)
