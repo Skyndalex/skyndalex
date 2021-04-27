312a8a9742aba9993d6a936f399545a0ca0c270d
Korrumz2PL <55253305+Korrumz2PL@users.noreply.github.com>
2021-04-27 11:01:59 +0200

    Update request.js

--- commands/bot/request.js
+++ commands/bot/request.js
@@ -1,6 +1,16 @@
-const Discord = require("discord.js")
+const Discord = require("discord.js");
+const cooldown = new Set;
 
 exports.run = async (client, message, args) => {
+    let requestModerators = [
+        "682572949219180547", // cyber
+        "829789505237024778", // inkatail
+        "494017032283619329", // rkubapl
+        "779643124578254858", // lexer
+    ]
+    if (cooldown.has(message.author.id)) {
+        client.error(message, "Poczekaj 5 minut zanim ponownie wyślesz request!")
+    } else {
   switch (args[0]) {
       case 'technic':
           let technicRequests = client.channels.cache.get("827522860489637908")
@@ -47,7 +57,21 @@ exports.run = async (client, message, args) => {
               .setColor("GREEN")
           message.channel.send(sentRequestBugs)
           break;
-      case 'default':
+      case 'suggest':
+          const suggestChannel = client.channels.cache.get("836363614699913227")
+
+          const embed4 = new Discord.MessageEmbed()
+              .setDescription(args.slice(1).join(" "))
+              .addField("Zgłosił", message.author.tag)
+              .addField("ID zgłaszającego", message.author.id)
+              .setColor("GREEN")
+          suggestChannel.send(embed4)
+
+          const sentRequestSuggest = new Discord.MessageEmbed()
+              .setDescription("Wysłano propozycję!")
+              .setColor("GREEN")
+          message.channel.send(sentRequestSuggest)
+          break;
       default:
           const embedDefault = new Discord.MessageEmbed()
               .setTitle("Zaawansowana pomoc")
@@ -55,11 +79,17 @@ exports.run = async (client, message, args) => {
               .addField("Pomoc techniczna", "\`request technic [treść]\`")
               .addField("Opinie", "\`request opinions [opinia]\`")
               .addField("Błędy", "\`request bugs [błąd]\`")
-              .setFooter(client.footer)
+              .setFooter(client.requestFooter)
               .setColor("GREEN")
           message.channel.send(embedDefault)
           break;
-  }
+          }
+     }
+  await cooldown.add(message.author.id)
+
+    setTimeout(async() => {
+       await cooldown.delete(message.author.id);
+    }, 300000);
 }
 exports.help = {
     name: "request",
