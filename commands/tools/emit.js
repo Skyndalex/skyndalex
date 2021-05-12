6dc957ccf0e6adbe1ca7170665bd02ac25fb91c9
Korrumz2PL <55253305+Korrumz2PL@users.noreply.github.com>
2021-05-12 13:28:05 +0200

    Update emit.js

--- commands/tools/emit.js
+++ commands/tools/emit.js
@@ -27,7 +27,7 @@ exports.run = async (client, message, args, level) => {
             let dev = ["817883855310684180"]
             if (!dev.includes(message.author.id)) return client.error(message, "Wymagane posiadanie permisji developera!!")
 
-            client.emit("guildCreate", message.member);
+            client.emit('guildMemberAdd', message.member)
 
             client.sender(message, "Wykonano pomyślnie!", "Wykonano event \`guildCreate\`", "", "GREEN", "", "")
 
@@ -46,8 +46,28 @@ exports.run = async (client, message, args, level) => {
                 }
             ])
             break;
+        case 'guildMemberRemove':
+            client.emit('guildMemberRemove', message.member)
+
+            client.sender(message, "Wykonano pomyślnie!", "Wykonano event \`guildMemberRemove\`", "", "GREEN", "", "")
+
+            client.authorSender(message, "Wykryto atywność!!", "Dane akcji poniżej", "", "GREEN", [
+                {
+                    name: "Serwer",
+                    value: message.guild.name
+                },
+                {
+                    name: "Użytkownik",
+                    value: message.author.tag
+                },
+                {
+                    name: "Nazwa akcji",
+                    value: "\`emit (event:guildMemberRemove)\`"
+                }
+            ])
+            break;
         default:
-            client.sender(message, "Co chcesz emitować?", "", "", "GREEN", [{name: "> \`guildMemberAdd\`", value: "Pozwala na uruchomienie eventu \`guildMemberAdd\`"}, {name: "> \`guildCreate\`", value: "Pozwala na uruchomienie eventu \`guildCreate\`"}])
+            client.sender(message, "Co chcesz emitować?", "", "", "GREEN", [{name: "> \`guildMemberAdd\`", value: "Pozwala na uruchomienie eventu \`guildMemberAdd\`"}, {name: "> \`guildCreate\`", value: "Pozwala na uruchomienie eventu \`guildCreate\`"}, {name: "> \`guildMemberRemove\`", value: "Pozwaala na emitowanie eventu \`guildRemoe\`"}])
     }
 }
 exports.help = {
