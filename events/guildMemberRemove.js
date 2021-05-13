8ccb0059100b56bc5b790fbafd9297bf7c81c192
Korrumz2PL <55253305+Korrumz2PL@users.noreply.github.com>
2021-05-13 12:02:50 +0200

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js")
 const r = require("rethinkdb")
 module.exports = async(client, member) => {
+    /*
     const channel = await r.table('settings').get(member.guild.id)("goodbyeChannel").run(client.con)
 
     const embed = new Discord.MessageEmbed()
@@ -8,4 +9,6 @@ module.exports = async(client, member) => {
         .setDescription(`Żegnaj, ${member.user.tag}. Mamy nadzieję że kiedyś do nas wrócisz`)
         .setColor("RED")
     member.guild.channels.cache.get(channel).send(embed)
+
+     */
 }
