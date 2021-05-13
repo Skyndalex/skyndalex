26de2e05a63a0b23e3680f982ef1941daf4ff940
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-05-13 12:42:36 +0200

    Update message.js

--- events/message.js
+++ events/message.js
@@ -2,21 +2,14 @@ const Discord = require('discord.js')
 const r = require("rethinkdb")
 const { prefix } = require("../config.json")
 const cooldown = new Set;
-
+const moment = require("moment")
+moment.locale("pl")
 module.exports = async(client, message) => {
     const embedMention = new Discord.MessageEmbed()
-        .setTitle("Witaj! Miło mi cię poznać")
-        .addField("Ostatnia aktualizacja bota", client.latestupdate)
-        .addField("Wersja bota", client.version)
-        .addField("Discord", client.discord)
-        .addField("Dokumentacja", client.docsLink)
-        .addField("Strona główna", client.url)
-        .addField("Status serwisów", client.statuspage)
-        .addField("Zalecane akcje (1)", client.PermissionsNotify)
-        .addField("Zalecane akcje (2)", client.securityNotify)
-        .addField("Prefix", prefix)
-        .setFooter(client.mentionFooter)
-        .setThumbnail(client.user.displayAvatarURL({size: 1024}))
+        .setTitle("Witaj!")
+        .addField("> Komenda pomocy", ";help")
+        .addField("> Uptime", require("moment").duration(client.uptime).humanize())
+        .addField("> Prefix", ";")
         .setColor("GREEN")
     const prefixMention = new RegExp(`^<@!?${client.user.id}>( |)$`);
     if (message.content.match(prefixMention)) {
@@ -24,25 +17,25 @@ module.exports = async(client, message) => {
             m.delete({timeout: 60000 })
         })
     }
-        if (message.author.bot) return;
+    if (message.author.bot) return;
 
-        if (!message.content.startsWith(prefix)) return
+    if (!message.content.startsWith(prefix)) return
 
-        const args = message.content.slice(prefix.length).trim().split(/ +/);
-        const command = args.shift().toLowerCase();
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
 
-        const embed = new Discord.MessageEmbed()
-            .setTitle("Użyto komendy")
-            .addField("Komenda", command)
-            .addField("Użył", message.author.tag)
-            .addField("ID serwera", message.guild.id)
-            .setColor("GREEN")
+    const embed = new Discord.MessageEmbed()
+        .setTitle("Użyto komendy")
+        .addField("Komenda", command)
+        .addField("Użył", message.author.tag)
+        .addField("ID serwera", message.guild.id)
+        .setColor("GREEN")
     client.channels.cache.get("832231792773431347").send(embed)
 
-        const cmd = client.commands.get(command) || client.commands.find(c => c.help.aliases && c.help.aliases == command);
-        if (!cmd) return;
-        const gban = await r.table("gbans").get(message.author.id).run(client.con)
-        if (gban) return client.error(message, `Otrzymałeś blokadę na korzystanie z komend`)
+    const cmd = client.commands.get(command) || client.commands.find(c => c.help.aliases && c.help.aliases == command);
+    if (!cmd) return;
+    const gban = await r.table("gbans").get(message.author.id).run(client.con)
+    if (gban) return client.error(message, `Otrzymałeś blokadę na korzystanie z komend`)
 
     let whitelist = ["817883855310684180"];
 
@@ -54,5 +47,5 @@ module.exports = async(client, message) => {
 
         cooldown.add(message.author.id);
         setTimeout(() => cooldown.delete(message.author.id), 2000);
-        }
-    };
+    }
+};
