0a31031fa1151b6c37c42966e181b67aabe9e70c
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-05-19 10:02:22 +0200

    Update repos.js

---
+++ commands/fun/repos.js
@@ -0,0 +1,39 @@
+const fetch = require("node-fetch")
+exports.run = async (client, message, args) => {
+    /*
+    https://api.github.com/users/Korrumz2PL/repos
+        data:
+     - id
+     - node_id
+     - name
+     - full_name
+     - private
+     - owner
+     - html_url
+     - description
+     - fork
+     - url
+     -
+     */
+    const user = args[0]
+    const repo = args[1]
+
+    if (!user) return message.channel.send("Brak użytkownika")
+    if (!repo) return message.channel.send("Nie podano repozytorium")
+
+    /*
+    fetch(`https://api.github.com/users/${user}/${repo}`)
+        .then(res => res.json())
+        .then(json => {
+            console.log(json.name)
+        })
+
+     */
+
+    message.channel.send("soon:tm:")
+}
+exports.help = {
+    name: "repos",
+    description: "Wyswietla statystyki repozytorium na platformie GitHub",
+    category: "fun",
+}
