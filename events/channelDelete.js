fd98d09bbf5741cabd8a26aa0c6ddb5ea8a0b9c6
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-05-24 16:34:33 +0200

    New channelDelete.js

---
+++ events/channelDelete.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js")
+const r = require("rethinkdb")
+module.exports = async (client, channel) => {
+    const logChannel = await r.table("settings").get(channel.guild.id)("channelDeleteLog").run(client.con)
+
+    if (channel.type === "category") {
+        const logEmbed1 = new Discord.MessageEmbed()
+            .setTitle("Usunięto kategorię!")
+            .addField("Nazwa", channel.name)
+            .addField("ID", channel.id)
+            .addField("Pozycja", channel.rawPosition)
+            .setColor("RED")
+        channel.guild.channels.cache.get(logChannel).send(logEmbed1)
+    } else {
+        let type = {
+            text: "Kanał tekstowy",
+            voice: "Kanał głosowy",
+            news: "Kanał ogłoszeniowy",
+            store: "Sklep",
+            unknown: "Nieznany typ"
+        }
+        let nsfw = {
+            true: "Tak",
+            false: "Nie"
+        }
+        const logEmbed2 = new Discord.MessageEmbed()
+            .setTitle("Usunięto kanał!")
+            .addField("Nazwa", channel.name)
+            .addField("ID", channel.id)
+            .addField("Pozycja", channel.rawPosition)
+            .addField("Typ", type[channel.type])
+            .addField("Bitrate", channel.bitrate||"Nie znaleziono danych")
+            .addField("Nsfw?", nsfw[channel.nsfw])
+            .addField("Limit użytkowników", channel.userLimit||"Brak/nie znaleziono informacji")
+            .addField("Temat kanału", channel.topic||"Nie znaleziono informacji")
+            .setColor("RED")
+        channel.guild.channels.cache.get(logChannel).send(logEmbed2)
+    }
+}
