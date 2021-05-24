bb5d09486068fdbbdcc8d1e96d8de46cbab457e5
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-05-24 12:01:59 +0200

    Update suggest.js

--- commands/tools/suggest.js
+++ commands/tools/suggest.js
@@ -11,6 +11,8 @@ exports.run = async (client, message, args, level) => {
         .setTitle("Opublikowano nową propozycję")
         .setDescription(args.join(" "))
         .setColor("GREEN")
+    if (message.attachments.map(a=>a.url)[0]) embed.setImage(message.attachments.map(a=>a.url)[0])
+    if (message.attachments.map(a=>a.url)[0]) embed.setFooter('W obrazku pokazane jest tylko jedno pierwsze zdjęcie!');
     client.channels.cache.get(channel).send(embed).then(m => {
         m.react("👍")
         m.react("👎")
