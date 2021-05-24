fba8a16ca73f651a322ab960874bbfe934a3e89a
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-05-24 16:34:50 +0200

    New emojiCreate.js

---
+++ events/emojiCreate.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js")
+const r = require("rethinkdb")
+module.exports = async (client, emoji) => {
+    const logChannel = await r.table("settings").get(emoji.guild.id)("emojiCreateLog").run(client.con)
+
+    let animated = {
+        true: "Tak",
+        false: "Nie"
+    }
+    const logEmbed = new Discord.MessageEmbed()
+        .setTitle("Utworzono emoji!")
+        .addField("Nazwa", emoji.name||"Nie znaleziono informacji")
+        .addField("ID", emoji.id||"Nie znaleziono informacji")
+        .addField("Animowana?", animated[emoji.animated]||"Nie znaleziono informacji")
+        .addField("URL", emoji.url||"Nie znaleziono informacji")
+        .setImage(emoji.url||null)
+        .setColor("GREEN")
+    emoji.guild.channels.cache.get(logChannel).send(logEmbed)
+}
