8e9aea253a1093640132db0be02b5e45d58f9ff5
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-05-24 20:37:01 +0200

    New emojiUpdate.js

--- events/emojiUpdate.js
+++ events/emojiUpdate.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js")
+const r = require("rethinkdb")
 module.exports = async (client, oldEmoji, newEmoji) => {
     const logChannel = await r.table('settings').get(oldEmoji.guild.id)("emojiUpdateLog").run(client.con)
 
@@ -6,4 +7,16 @@ module.exports = async (client, oldEmoji, newEmoji) => {
         true: "Tak",
         false: "Nie"
     }
+
+    const logEmbed = new Discord.MessageEmbed()
+        .setTitle("Edytowano emoji")
+        .addField("Nazwa przed", oldEmoji.name)
+        .addField("Nazwa po", newEmoji.name)
+        .addField("ID", oldEmoji.id)
+        .addField("Animowana?", animated[newEmoji.animated])
+        .setImage(newEmoji.url)
+        .setColor("#1aebc4")
+    newEmoji.guild.channels.cache.get(logChannel).send(logEmbed)
+
+    //////////
 }
