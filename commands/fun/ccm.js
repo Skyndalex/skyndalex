441eb040c5201f719ee5d46be2db3dc97995cd90
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-05-24 12:01:27 +0200

    Update ccm.js

--- commands/fun/ccm.js
+++ commands/fun/ccm.js
@@ -1,30 +1,29 @@
-const { createCanvas, loadImage } = require("canvas")
-const discord = require("discord.js")
+const { createCanvas, loadImage } = require('canvas')
+const canvas = createCanvas(200, 200)
+const ctx = canvas.getContext('2d')
 exports.run = async (client, message, args) => {
     const canvas = createCanvas(200, 200)
     const ctx = canvas.getContext('2d')
 
     ctx.font = '30px Impact'
     ctx.rotate(0.1)
-    ctx.fillText('Hello world!', 50, 100)
+    ctx.fillText('Awesome!', 50, 100)
 
-    let text = ctx.measureText("Hello world!")
-
+    let text = ctx.measureText('Awesome!')
     ctx.strokeStyle = 'rgba(0,0,0,0.5)'
     ctx.beginPath()
     ctx.lineTo(50, 102)
     ctx.lineTo(50 + text.width, 102)
     ctx.stroke()
 
- const img = await loadImage('./commands/fun/funResources/img/ccm.png').then((image) => {
+    loadImage('../fun/changemymind.jpg').then((image) => {
         ctx.drawImage(image, 50, 0, 70, 70)
 
-     message.channel.send(img)
-
+        console.log('<img src="' + canvas.toDataURL() + '" />')
     })
 }
 exports.help = {
     name: "ccm",
-    description: "Wysyła podanie",
+    description: "Generuje tekst changemymind",
     category: "fun",
 }
