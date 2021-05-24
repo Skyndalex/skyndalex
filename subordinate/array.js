0f314d5a91921ce12552625792d46d8119e9c4f8
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-05-24 10:48:43 +0200

    New array.js

---
+++ subordinate/array.js
@@ -0,0 +1,34 @@
+module.exports = async(client) => {
+    // soontm zrobie szukanie po roli na serwerze
+
+    client.specialUsersArray = [
+        "817883855310684180",
+        "304979757852917762"
+    ]
+
+    client.premiumUsersArray = [
+        ""
+    ]
+
+    client.moderatorsUsersArray = [
+        "375247025643716609",
+        "829789505237024778",
+        "494017032283619329",
+        "779643124578254858"
+    ]
+
+    client.headadminUsersArray = [
+        "304979757852917762",
+        "682572949219180547"
+    ]
+
+    client.generalModerators = [
+        "817883855310684180",
+        "304979757852917762",
+        "682572949219180547",
+        "375247025643716609",
+        "829789505237024778",
+        "494017032283619329",
+        "779643124578254858"
+    ]
+}
