ec91383ff5091b17821915134e3612dbc4b4f60c
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-05-26 17:22:31 +0200

    Update przytul.js

--- commands/fun/przytul.js
+++ commands/fun/przytul.js
@@ -1,4 +1,3 @@
-const Discord = require("discord.js")
 exports.run = (client, message, args) => {
     const user = message.mentions.users.first()||client.users.cache.get(args[0])
     if (!user) return client.error(message, "Nie znaleziono uzytkownika!")
