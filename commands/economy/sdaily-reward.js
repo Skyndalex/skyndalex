5cc2960113b5b720a5a932d5cdc2b2019a18963f
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-05-26 17:22:51 +0200

    Delete sdaily-reward.js

--- commands/economy/sdaily-reward.js
+++
@@ -1,8 +0,0 @@
-exports.run = async (client, message, args) => {
-    client.commandNotEnabled(message, "Przeniesione do wersji 3.1")
-}
-exports.help = {
-    name: "sdaily-reward",
-    aliases: [],
-    category: "economy",
-}
