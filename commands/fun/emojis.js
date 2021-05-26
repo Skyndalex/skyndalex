ad0ca7555683b19a784c1d118055b497103c9b76
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-05-26 17:17:58 +0200

    Delete emojis.js

--- commands/fun/emojis.js
+++ commands/fun/emojis.js
@@ -1,4 +1,3 @@
-const Discord = require("discord.js");
 exports.run = async (client, message, args) => {
     if (!args[0]) return client.error(message, `Nie podano tekstu!`)
     let text = args.splice('').join(' ')
