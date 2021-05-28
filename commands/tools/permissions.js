ebc1d4962a8d57009f6f58f40f4acc0bb707786a
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-05-28 16:59:08 +0200

    Update permissions.js

--- commands/tools/permissions.js
+++ commands/tools/permissions.js
@@ -8,6 +8,46 @@ exports.run = async (client, message, args) => {
                 }
             ])
             break;
+        case 'server-mods':
+            client.sender(message, "Lista permisji -> server-mods", "Menu: \`server\`", "UWAGA: Permisje są w wczesnej wersji!", "GREEN", [
+                {
+                    name: "Lista",
+                    value: client.serverModerationPermissionsArray
+                }
+            ])
+            break;
+        case 'server-fun':
+            client.sender(message, "Lista permisji -> server-fun", "Menu: \`server\`", "UWAGA! Permisje są w wczesnej wersji!", "GREEN", [
+                {
+                    name: "Lista",
+                    value: client.serverFunPermissionsArray
+                }
+            ])
+            break;
+        case 'server-tools':
+            client.sender(message, "Lista permisji -> server.tools", "Menu: \`server\`", "UWAGA! Permisje są w wczesnej wersji", "GREEN", [
+                {
+                    name: "Lista",
+                    value: client.serverToolsPermissionsArray
+                }
+            ])
+            break;
+        case 'global-economy':
+            client.sender(message, "Lista permisji -> global.economy", "Menu: \`global\`", "UWAGA! Permisje są w wczesnej wersji", "GREEN", [
+                {
+                    name: "Lista",
+                    value: client.serverEconomyPermissionsArray
+                }
+            ])
+            break;
+        case 'server-vc':
+            client.sender(message, "Lista permisji -> server.vc", "Menu: \`server\`", "UWAGA! Permisje są w wczesnej wersji", "GREEN", [
+                {
+                    name: "Lista",
+                    value: client.serverVCmanagementPermissionsArray
+                }
+            ])
+            break;
         default:
             client.sender(message, "Pomoc do permisji użytkowników", "", "", "GREEN", [
                 {
@@ -25,6 +65,26 @@ exports.run = async (client, message, args) => {
                 {
                     name: "\`permissions global\`",
                     value: "Lista globalnych permisji"
+                },
+                {
+                    name: "\`permissions server-mods\`",
+                    value: "Lista permisji moderatorskich"
+                },
+                {
+                    name: "\`permissions server-fun\`",
+                    value: "Lista permisji do komend 4Fun"
+                },
+                {
+                    name: "\`permissions server-tools",
+                    value: "Lista permisji do komend Tools"
+                },
+                {
+                    name: "\`permissions global-economy\`",
+                    value: "Lista permisji do globalnej ekonomii"
+                },
+                {
+                    name: "\`permissions server-vc\`",
+                    value: "Lista permisji do komend Zarządzania kanałami VC"
                 },
                 {
                     name: "\`permisions check [permisja]\`",
