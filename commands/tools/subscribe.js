d51e330ef8ee63ef4105be163ba253527b971c41
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-06-05 22:30:58 +0200

    Yt notifications

---
+++ commands/tools/subscribe.js
@@ -0,0 +1,12 @@
+const r = require("rethinkdb")
+exports.run = async (client, message, args) => {
+/*
+    const youtubeChannel = args.join("").slice(0)
+    if (!youtubeChannel) return message.channel.send("Nie podano kanału YouTube!")
+
+    await r.table("notifications").update({youtubeChannel: youtubeChannel}).run(client.con)
+
+    client.sender(message, "Zasubkrybowano!", `Zasubskrybowano kanał ${youtubeChannel} pomyślnie!\n\nINFORMACJA: Powiadomienia YouTube są w wersji wczesnej Alphy i nie działają poprawnie!!!`, "", "GREEN", "", "", "")
+
+ */
+}
