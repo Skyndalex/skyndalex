5f1ec1fc4676c7badabdc51d6b4ecec4dacce6ec
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-06-05 21:12:43 +0200

    Update stats.js

--- commands/bot/stats.js
+++ commands/bot/stats.js
@@ -3,19 +3,20 @@ const os = require("os")
 const moment = require("moment")
 moment.locale("PL")
 exports.run = async (client, message, args) => {
-    let statEmbed = new Discord.MessageEmbed()
-        .setColor('GREEN')
-        .setTitle(`Statystyki`)
-        .setDescription(`**Uptime: ${require("moment").duration(client.uptime).humanize()}**`)
-        .addField(`Liczba serwerów`, `${client.guilds.cache.size}`, true)
-        .addField(`Liczba użytkowników`,`${client.users.cache.size}`, true)
-        .addField(`Liczba kanałów`, `${client.channels.cache.size}`, true)
-        .addField(`Użycie pamięci RAM`, `${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)}/${(os.totalmem() / 1024 / 1024).toFixed(2)} MB`, true)
-        .addField('Wersja discord.js', `${Discord.version}`, true)
-        .addField(`Wersja node.js`, `${process.version}`, true)
-        .addField("Procesor", require("os").cpus().map((i) => i.model)[0], true)
-        .addField("System operacyjny", "linux", true)
-    message.channel.send(statEmbed)
+    client.sender(message, "Statystyki", "", "", "GREEN", [
+        {
+            name: "> \`Zasoby:\`",
+            value: `Użycie pamięci RAM: \`${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)}/${(os.totalmem() / 1024 / 1024).toFixed(2)} MB\`\n Wersja Node.Js:\`${process.version}\`\nWersja Discord.Js: \`${Discord.version}\`\nProcesor: \`${require("os").cpus().map((i) => i.model)[0]}\`\nSystem operacyjny: \`Linux\``
+        },
+        {
+            name: "> \`Cache:\`",
+            value: `Liczba serwerów: \`${client.guilds.cache.size}\`\nLicza użytkowników: \`${client.users.cache.size}\`\nLiczba kanałów: \`${client.channels.cache.size}\`\nLiczba emotek: \`${client.emojis.cache.size}\``
+        },
+        {
+            name: "> \`Uptime:\`",
+            value: require("moment").duration(client.uptime).humanize()
+        }
+    ])
 
 }
 exports.help = {
