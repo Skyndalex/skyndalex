b723f17c0bff316c8cb7d1f82b35ac9d3f0bffcc
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-06-12 16:47:26 +0200

    New file structure [ALPHA]

--- events/ready.js
+++
@@ -1,4 +0,0 @@
-module.exports = (client) => {
-    let statuses = "KriveBot - front-end, innovation, professional update v4.0"
-    setInterval(() => client.user.setActivity(statuses, {type: "PLAYING"}), 10000)
-}
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const Discord = require("discord.js")
 const fs = require("fs")
-const { token } = require("./config.json")
 const KriveManager = require("./Client.js")
 const r = require("rethinkdb")
 const client = new KriveManager()
 const express = require('express')
 const app = express()
 const port = 2222
+const { token } = require("./config.json")
 
-require("./dashboard/dashboard").run(client);
-require("./games/games").run(client)
-require("./func.js")(client);
-require("./subordinate/array")(client)
-require("./subordinate/apikeys")(client)
-require("./site-new/static").run(client)
+require("./src/dashboard/dashboard").run(client);
+require("./src/games/games").run(client)
+require("./func")(client);
+require("./src/subordinate/array")(client)
+require("./src/subordinate/apikeys")(client)
+require("./src/site-new/static").run(client)
 app.get('/', (req, res) => {
 	res.send("Connected")
 })
@@ -29,9 +29,9 @@ client.commands = new Discord.Collection();
 // client.events = new Discord.Collection()
 
 fs.readdirSync("./commands/").forEach(dir => {
-	const commands = fs.readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
+	const commands = fs.readdirSync(`./src/commands/${dir}/`).filter(file => file.endsWith(".js"));
 	for (const file of commands) {
-		let pull = require(`./commands/${dir}/${file}`);
+		let pull = require(`./src/commands/${dir}/${file}`);
 		if (pull.help && pull.help.name) {
 			client.commands.set(pull.help.name, pull);
 		} else {
@@ -39,9 +39,9 @@ fs.readdirSync("./commands/").forEach(dir => {
 		}
 	}
 });
-	const eventFiles = fs.readdirSync("./events").filter(file => file.endsWith(".js"));
+	const eventFiles = fs.readdirSync("./src/events/").filter(file => file.endsWith(".js"));
 	for (const file of eventFiles) {
-		const event = require(`./events/${file}`);
+		const event = require(`./src/events/${file}`);
 		const eventName = file.split(".")[0];
 		client.on(eventName, event.bind(null, client))
 	}
