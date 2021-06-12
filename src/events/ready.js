37e72d2c686c1fb2e3f62e19bb9f98dd277b2c44
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-06-13 00:23:29 +0200

    New statuses

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,4 +1,4 @@
 module.exports = (client) => {
-    let statuses = "KriveBot - front-end, innovation, professional update v4.0"
-    setInterval(() => client.user.setActivity(statuses, {type: "PLAYING"}), 10000)
+    let statuses = ["KriveBot - front-end, innovation, professional update v4.0", `Dodaj mnie już teraz! → ${client.url}/invite`, `Dołącz na serwer support! → ${client.url}/discord`, `Nowa strona internetowa! → ${client.url}`]
+    setInterval(() => client.user.setActivity(statuses.random(), {type: "PLAYING"}), 10000)
 }
