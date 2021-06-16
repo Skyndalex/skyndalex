0cfb4b742f8a789cd3110c99ad8ef16c7f0c56d2
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-06-16 22:14:24 +0200

    New howsimp.js

---
+++ src/commands/fun/howsimp.js
@@ -0,0 +1,13 @@
+exports.run = async (client, message, args) => {
+    let percent = Math.floor(Math.random() * (100 - 0) + 0)
+
+    if (!args[0]) return client.sender(message, "401: No content.", "Nie podano argumentów", client.footer, "RED", "", "")
+
+    client.sender(message, "", `${args[0]} jest simpem na ${percent}%`, "", "GREEN", "", "")
+
+}
+exports.help = {
+    name: "howsimp",
+    description: "Komenda sprawdzająca, jak bardzo ktoś jest SIMPem",
+    category: "fun",
+}
