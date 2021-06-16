cd835e8d56a854b9d811ee01f2d0e0276d8417b6
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-06-16 22:14:15 +0200

    New hack.js

---
+++ src/commands/fun/hack.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js')
+exports.run = async (client, message, args) => {
+    if (!args[0]) return client.sender(message, "401: No content.", "Nie podano argumentów. UwAżAj!", client.footer, "RED", "", "")
+
+    const ip1 = Math.floor(Math.random() * (200 - 0) + 0)
+    const ip2 = Math.floor(Math.random() * (201 - 0) + 0)
+    const ip3 = Math.floor(Math.random() * (202 - 0) + 0)
+    const ip4 = Math.floor(Math.random() * (203 - 0) + 0)
+
+    const password = `bla123${Math.floor(Math.random() * (10000 - 0) + 0)}blabla (no scam)`
+
+    const logins = ["pasztetowo123", "minecrafterrr10003", "MrBoombastic", "Korrumz2", "ŚwinkaMorska320", "TwójDarmowyHacker10391", "JestemZaPIS"]
+    const emails = ["nI@gmail.com", "secret@krivebot.xyz", "support@krivebot.xyz", "verifikejszyn@gmail.com", "email102020@gmail.com"]
+
+    const embed1 = new Discord.MessageEmbed()
+        .setTitle("Hackowanie!")
+        .setDescription("Pobieram dane...")
+        .setColor("GREEN")
+
+    const data = new Discord.MessageEmbed()
+        .setTitle("Hackowanie!")
+        .setDescription("Pobrano dane!")
+        .addField("Adres IP", `${ip1}.${ip2}.${ip3}.${ip4}`)
+        .addField("Hasło", password)
+        .addField("Login", logins.random())
+        .addField("Email", emails.random())
+        .addField("Discord tag", args[0])
+        .setColor("GREEN")
+    message.channel.send(embed1).then(edits => {
+        edits.edit(data)
+    })
+}
+exports.help = {
+    name: "hack",
+    description: "Zhakuj kogoś ::::D",
+    category: "fun",
+}
