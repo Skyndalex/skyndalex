bb9b3ae6c51f162a027d953bc0e0ca5201dc8606
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-06-17 23:22:03 +0200

    new tables

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -5,20 +5,21 @@ module.exports = async(client, guild) => {
         id: guild.id
     }).run(client.con)
 
-   await r.table("moderation").insert({
-       id: guild.id
-   }).run(client.con)
-/*
-    await r.table("ServerEconomy").insert({
+    await r.table("moderation").insert({
         id: guild.id
     }).run(client.con)
-
- */
 
     await r.table("system").insert({
         id: guild.id
     }).run(client.con)
+
     await r.table("notifications").insert({
+        id: guild.id
+    }).run(client.con)
+    await r.table("giveaways").insert({
+        id: guild.id
+    }).run(client.con)
+    await r.table("logs").insert({
         id: guild.id
     }).run(client.con)
 }
