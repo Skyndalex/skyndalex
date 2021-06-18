87970e18f5bc7e594f590bef95903d7782e66c5c
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-06-18 18:09:22 +0200

    Help rebuildingK

--- src/commands/bot/help.js
+++ src/commands/bot/help.js
@@ -8,18 +8,19 @@ exports.run = (client, message, args) => {
         client.sender(message, "Informacje o komendzie", `Nazwa komendy: **${command.help.name}**`, "", "GREEN", [
             {
                 name: "> Opis komendy:",
-                value: command.help.description||"Brak"
+                value: command.help.description || "Brak"
             },
             {
                 name: "> Kategoria",
-                value: command.help.category||"Brak"
+                value: command.help.category || "Brak"
             },
             {
                 name: "> Nazwa komendy",
-                value: command.help.name||"Brak"
+                value: command.help.name || "Brak"
             }
         ])
     } else {
+        /*
         client.sender(message, `Pomoc (${client.commands.size})`, `Potrzebujesz pomocy? Wejdź na nasz [support](${client.url}/discord)\nPotrzebujesz pomocy z ustawianiem? Zobacz naszą [dokumentację](https://docs.krivebot.xyz)`, "Aby zobaczyć więcej informacji o komendzie, użyj komendy help [nazwa]", "GREEN", [
             {
                 name: "Bot",
@@ -54,6 +55,71 @@ exports.run = (client, message, args) => {
                 value: `> \`${client.commands.filter(c => c.help.category === "vcmanagement").map(c => c.help.name).join(" | ") || "Brak"}\``
             }
         ])
+    }
+
+         */
+        switch (args[0]) {
+            default:
+                client.sender(message, `Komendy`, `Ilość dostępnych komend: ${client.commands.size}\nDokumentacja: [link](https://docs.krivebot.xyz)\nStrona internetowa: [link](https://krivebot.xyz)`, `Potrzebujesz dodatkowej pomocy? Uzyj komendy ;ticket lub ;request!`, "GREEN", [
+                    {
+                        name: "Kat: Bot",
+                        value: "> \`help bot\`"
+                    },
+                    {
+                        name: "Kat: Fun (Zabawa)",
+                        value: "> \`help fun\`"
+                    },
+                    {
+                        name: "Kat: Moderation (moderacja)",
+                        value: "> \`help moderation\`"
+                    },
+                    {
+                        name: "Kat: tools (Narzędzia)",
+                        value: "> \`help tools\`"
+                    },
+                    {
+                        name: "Kat: Developers (Developerzy)",
+                        value: "> \`help devs\`"
+                    },
+                    {
+                        name: "Kat: economy (Ekonomia)",
+                        value: "> \`help economy\`"
+                    },
+                    {
+                        name: "Kat: VC managememt (Zarządzanie VC)",
+                        value: "> \`help vc\`"
+                    },
+                    {
+                        name: "Kat: NSFW",
+                        value: "> \`help nsfw\`"
+                    }
+                ])
+                break;
+            case 'bot':
+                client.sender(message, "Widzisz kategorię: Bot", `> \`${client.commands.filter(c => c.help.category === "bot").map(c => c.help.name).join(' | ') || "Brak"}\``, "", "GREEN", "", "")
+                break;
+            case 'fun':
+                client.sender(message, "Widzisz kategorię: Fun", `> \`${client.commands.filter(c => c.help.category === "fun").map(c => c.help.name).join(' | ') || "Brak"}\``, "", "GREEN", "", "")
+                break;
+            case 'moderation':
+                client.sender(message, "Widzisz kategorię: Moderation", `> \`${client.commands.filter(c => c.help.category === "moderation").map(c => c.help.name).join(' | ') || "Brak"}\``, "", "GREEN", "", "")
+                break;
+            case 'tools':
+                client.sender(message, "Widzisz kategorię: Tools", `> \`${client.commands.filter(c => c.help.category === "tools").map(c => c.help.name).join(' | ') || "Brak"}\``, "", "GREEN", "", "")
+                break;
+            case 'devs':
+                client.sender(message, "Widzisz kategorię: Developers", `> \`${client.commands.filter(c => c.help.category === "developers").map(c => c.help.name).join(' | ') || "Brak"}\``, "", "GREEN", "", "")
+                break;
+            case 'economy':
+                client.sender(message, "Widzisz kategorię: Economy", `> \`${client.commands.filter(c => c.help.category === "economy").map(c => c.help.name).join(' | ') || "Brak"}\``, "", "GREEN", "", "")
+                break;
+            case 'vc':
+                client.sender(message, "Widzisz kategorię: VC", `> \`${client.commands.filter(c => c.help.category === "vcmanagement").map(c => c.help.name).join(' | ') || "Brak"}\``, "", "GREEN", "", "")
+                break;
+            case 'nsfw':
+                client.sender(message, "Widzisz kategorię: Nsfw", `> \`${client.commands.filter(c => c.help.category === "nsfw").map(c => c.help.name).join(' | ') || "Brak"}\``, "", "GREEN", "", "")
+                break;
+        }
     }
 }
 exports.help = {
