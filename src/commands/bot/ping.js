bb285338893a3dab7c386d43d61f05c2031ad55b
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-06-18 18:09:43 +0200

    Ping commands update

--- src/commands/bot/ping.js
+++ src/commands/bot/ping.js
@@ -1,5 +1,5 @@
 exports.run = (client, message) => {
-    client.sender(message, "", `Opóźnienie bota: ${client.ws.ping}ms\nOpóźnienie API: ${Date.now() - message.createdTimestamp}ms`, "", "GREEN", "", "")
+    client.sender(message, "", `[Status](https://status.krivebot.xyz) naszych wszystkich serwisów\n\nOpóźnienie bota: \`${client.ws.ping}ms\`\nOpóźnienie API: \`${Date.now() - message.createdTimestamp}ms\``, "", "GREEN", "", "")
 };
 
 exports.help = {
