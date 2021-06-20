4698f0b2018be77bcd6da4c41f0a971cff9bb8fe
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-06-20 16:56:53 +0200

    Complaint logs

--- src/commands/tools/complaint.js
+++ src/commands/tools/complaint.js
@@ -15,12 +15,25 @@ exports.run = async (client, message, args) => {
         .addField("Zgłoszony użytkownik", user)
         .addField("Powód", reason)
         .addField("Zgłosił", message.author.tag)
-        .setColor("GREEN")
+        .setColor("RED")
     if (message.attachments.map(a=>a.url)[0]) embed.setImage(message.attachments.map(a=>a.url)[0])
     if (message.attachments.map(a=>a.url)[0]) embed.setFooter('W obrazku pokazane jest tylko jedno pierwsze zdjęcie!');
     client.channels.cache.get(channel).send(embed)
 
     client.sender(message, `Wysłałeś skargę pomyślnie!`, `Wysłano skargę na użytkownika ${user}\nZ powodu: ${reason}\nKanał: <#${channel}>`, ``, `GREEN`)
+
+    const logChannel = await r.table("logs").get(message.guild.id)("complaintLog").run(client.con)
+    if (!logChannel) return message.channel.send("Nie ustawiono logów skarg, więc nie jestem w stanie przekierować je na kanał z logami!").then(m => {
+        m.delete({timeout: 1000})
+    })
+
+    const embedComplaintPing = new Discord.MessageEmbed()
+    .setTitle("Logi: Wysłano skargę.")
+    .addField("Oskarżony użytkownik", user)
+    .addField("Powód", reason)
+    .addField("Zgłosił", message.author.tag)
+    .setColor("GREEN")
+    client.channels.cache.get(logChannel).send(embedComplaintPing)
 }
 exports.help = {
     name: "complaint",
