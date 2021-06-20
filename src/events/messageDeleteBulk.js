71e20b587b6dba76e92aaef1a88f558d4aa92603
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-06-20 16:57:28 +0200

    update messageDeleteBulk.js

--- src/events/messageDeleteBulk.js
+++ src/events/messageDeleteBulk.js
@@ -2,6 +2,7 @@ const r = require("rethinkdb")
 const Discord = require("discord.js")
 const hastebin = require("hastebin")
 module.exports = async (client, message) => {
+    /*
     const logChannel = await r.table('settings').get(message.map(m=>m)[1].channel.guild.id)("messageDeleteBulkLog").run(client.con)
 
     hastebin.createPaste(`Deleted ${message.size} messages in channel ${message.map(m => m)[1].channel.name}\n\nList: ${+message.map(m => `Author: ${m.author.tag} (${m.author.id}) Message ID: ${m.id} Content:\\n${m.content}\`).join(\`\\n\`)`)}`, {
@@ -12,7 +13,8 @@ module.exports = async (client, message) => {
         .catch(function (requestError) {})
         const embed = new Discord.MessageEmbed()
             .setTitle("Usunięto wiele wiadomości!")
-            .addField("Link", "NOT FOR DOG")
+            .addField("Link", "SOON")
             .setColor("GREEN")
     message.guild.channels.cache.get(logChannel).send(embed)
+    */
 }
