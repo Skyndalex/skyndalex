973bee985bdb8eb3ab683dd731ed61c21fef739e
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-07-01 14:30:07 +0200

    Redirects

--- src/botsite/static.js
+++ src/botsite/static.js
@@ -10,6 +10,17 @@ exports.run = (client) => {
             bot: client
         })
     })
+
+    app.get('/invite', function(req, res) {
+        res.redirect('https://discord.com/oauth2/authorize?client_id=836529470122622986&scope=bot&permissions=0');
+    });
+    app.get('/discord', function(req, res) {
+        res.redirect('https://discord.gg/YCxCQ2ZZhA');
+    });
+    app.get('/public', function(req, res) {
+        res.redirect('https://github.com/Korrumz2PL/krivebot');
+    });
+    
     app.get('*', function(req, res){
         res.render('./views/src/404.ejs')
     });
