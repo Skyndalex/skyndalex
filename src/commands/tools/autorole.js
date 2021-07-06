493358291f18737629c9573a522bc8bbcba319b9
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-07-06 22:36:17 +0200

    Auto-Role

--- src/commands/tools/autorole.js
+++ src/commands/tools/autorole.js
@@ -1,27 +1,50 @@
 const r = require("rethinkdb")
 exports.run = async (client, message, args) => {
+    const guild = await r.table("autorole").get(message.guild.id).run(client.con)
+
     switch (args[0]) {
         default:
-            const roleList = await r.table("autorole").get(message.guild.id)("role").run(client.con)
-
             client.sender(message, "Auto-Role", "Witaj w menu autoroli! Możesz tutaj dodać role które użytkownik będzie dostawał automatycznie.", "UWAGA! - Nie da się dodać kolejnej roli.", "GREEN", [
                 {
                     name: "> Lista",
-                    value: `<@&${roleList}>`
+                    value: `<@&${guild.role}>`
                 },
                 {
                     name: "> Dodawanie roli",
-                    value: "autorole add [oznaczenie]"
+                    value: "autorole \`add [oznaczenie]\`"
                 },
+                {
+                    name: "Włączanie/Wyłączanie",
+                    value: "autorole \`activate/deactivate\`"
+                }
             ])
             break;
-            case 'add':
+               case 'add':
+                if (!guild?.activate) return message.channel.send("Autorole są wyłączone!")
+
                 const role = message.guild.roles.cache.get(args[0]) || message.mentions.roles.first()
+                if (!role) return message.channel.send("Nie podano roli.")
 
                 const updated = await r.table("autorole").update({role: role.id}).run(client.con)
+                if (!updated) return message.channel.send("Nie aktywowano autoroli!")
 
                 message.channel.send("Zaktualizowano role!")
                 break;
+                case 'activate':
+                    if (!guild) await r.table("autorole").insert({
+                        id: message.guild.id,
+                        activate: true
+                    }).run(client.con)
+                    
+                    r.table("autorole").update({activate: true}).run(client.con)
+                    
+                    client.sender(message, "Aktualizacja ustawień", "Włączono autorole!", "", "GREEN", "", "")
+                    break;
+                case 'deactivate':
+                    await r.table("autorole").get(message.guild.id).update({activate: false}).run(client.con)
+                    
+                    client.sender(message, "Aktualizacja ustawień", "Wyłączono autorole!", "", "RED", "", "")
+                    break;
     }
 };
 
