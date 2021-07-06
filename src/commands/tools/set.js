ebecdcae41babb9ee68fc5b41cdb031a73e99608
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-07-06 21:24:33 +0200

    set menu

--- src/commands/tools/set.js
+++ src/commands/tools/set.js
@@ -1,6 +1,33 @@
 exports.run = (client, message, args) => {
     switch (args[0]) {
-        
+        default: 
+        client.sender(message, "Ustawienia serwerowe - menu", "Witaj w ustawieniach serwerowych!\n[Dokumentacja](https://docs.krivebot.xyz)", "", "GREEN", [
+            {
+                name: "Ustawienia kanałów",
+                value: "\`set ch\`"
+            },
+            {
+                name: "Ustawienia ról",
+                value: "\`set roles\`"
+            },
+            {
+                name: "Ustawienia bota",
+                value: "\`set bot\`"
+            },
+            {
+                name: "Ustawienia logów",
+                value: "\`logs\`"
+            }
+        ])
+        break;
+        case 'ch':
+            client.sender(message, "Ustawienia - kanały", "Lista ustawień kanałów [PORADNIK](https://docs.krivebot.xyz)", "", "GREEN", [
+                {
+                    name: "Kanał ogłoszeń",
+                    value: "BRAK."
+                }
+            ])
+            break;
     }
 };
 
