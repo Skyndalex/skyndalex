49fdce6147ca69332931ac5ab4453d6e802c31a2
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-07-07 23:17:27 +0200

    ```Update set.js```

--- src/commands/tools/set.js
+++ src/commands/tools/set.js
@@ -1,4 +1,7 @@
-exports.run = (client, message, args) => {
+const r = require("rethinkdb")
+exports.run = async (client, message, args) => {
+    if (!message.member.hasPermission("MANAGE_SERVER")) return client.sender(message, "Nie możesz tego użyć!", "Brak odpowiednich permisji:\n\`server.admin.settings\`.\nJeśli uważasz, że to błąd skontaktuj się z administratorem serwera/bota", "", "RED", "", "")
+
     switch (args[0]) {
         default: 
         client.sender(message, "Ustawienia serwerowe - menu", "Witaj w ustawieniach serwerowych!\n[Dokumentacja](https://docs.krivebot.xyz)", "", "GREEN", [
@@ -21,13 +24,56 @@ exports.run = (client, message, args) => {
         ])
         break;
         case 'ch':
+            const g = await r.table("settings").get(message.guild.id).run(client.con)
+
             client.sender(message, "Ustawienia - kanały", "Lista ustawień kanałów [PORADNIK](https://docs.krivebot.xyz)", "", "GREEN", [
                 {
                     name: "Kanał ogłoszeń",
-                    value: "BRAK."
+                    value: `<#${g.broadcastChannel||"Brak"}>`
+                },
+                {
+                    name: "Kanał obrazkowy",
+                    value: `<#${g.mediaOnlyChannel||"Brak"}>`
+                },
+                {
+                    name: "Kanał memów",
+                    value: `<#${g.memeChannel||"Brak"}>`
                 }
             ])
             break;
+        case 'broadcastChannel':
+            const broadcastChannelEnable = await r.table("settings").get(message.guild.id).run(client.con)
+            if (!broadcastChannelEnable?.broadcastActivate) return message.channel.send("Kanały ogłoszeń są wyłączone! Proszę je włączyć komendą \`activate\`!")
+
+            let bChannel = message.guild.channels.cache.find( c => c.name.toLowerCase().includes(args[1].toLowerCase())) || message.guild.channels.cache.get(args[1]) || message.mentions.channels.first()
+            if (!bChannel) return client.sender(message, "Błąd!", "Nie znaleziono kanału!", "", "RED", "", "")
+
+            const update1 = await r.table("settings").get(message.guild.id).update({broadcastChannel: bChannel.id}).run(client.con)
+
+            client.sender(message, "Zaktualizowano!", "Pomyślnie ustawiono: \`broadcastChannel\` (Kanał ogłoszeń).", client.set, "GREEN", "", "")
+            break;
+            case 'mediaOnlyChannel':
+                const mediaOnlyChannelEnable = await r.table("settings").get(message.guild.id).run(client.con)
+                if (!mediaOnlyChannelEnable?.mediaOnlyActivate) return message.channel.send("Kanały obrazkowe są wyłączone! Proszę je włączyć komendą \`activate\`!")
+    
+                let moChannel = message.guild.channels.cache.find( c => c.name.toLowerCase().includes(args[1].toLowerCase())) || message.guild.channels.cache.get(args[1]) || message.mentions.channels.first()
+                if (!moChannel) return client.sender(message, "Błąd!", "Nie znaleziono kanału!", "", "RED", "", "")
+    
+                const update2 = await r.table("settings").get(message.guild.id).update({mediaOnlyChannel: moChannel.id}).run(client.con)
+    
+                client.sender(message, "Zaktualizowano!", "Pomyślnie ustawiono: \`mediaOnlyChannel\` (Kanał obrazkowy).", client.set, "GREEN", "", "")
+                break;
+                case 'memeChannel':
+                    const memeChannelEnable = await r.table("settings").get(message.guild.id).run(client.con)
+                    if (!memeChannelEnable?.memeChannelActivate) return message.channel.send("Kanał memów jest wyłączony! Proszę je włączyć komendą \`activate\`!")
+        
+                    let mChannel = message.guild.channels.cache.find( c => c.name.toLowerCase().includes(args[1].toLowerCase())) || message.guild.channels.cache.get(args[1]) || message.mentions.channels.first()
+                    if (!mChannel) return client.sender(message, "Błąd!", "Nie znaleziono kanału!", "", "RED", "", "")
+        
+                    const update3 = await r.table("settings").get(message.guild.id).update({memeChannel: mChannel.id}).run(client.con)
+        
+                    client.sender(message, "Zaktualizowano!", "Pomyślnie ustawiono: \`memeChannel\` (Kanał do memów).", client.set, "GREEN", "", "")
+                    break;
     }
 };
 
