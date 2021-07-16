cf99a4ee33f50289484990dd4fb3a5c7daa74af5
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-07-16 02:35:30 +0200

    h

--- src/commands/tools/permissions.js
+++ src/commands/tools/permissions.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args) => {
-    message.channel.send("[Lista permisji została przeniesiona do dokumentacji](https://docs.krivebot.xyz/pl/permissions)")
+    message.channel.send("Lista permisji została przeniesiona do dokumentacji: https://docs.krivebot.xyz/pl/permissions")
 };
 
 exports.help = {
