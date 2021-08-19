6ab7245ff244be3c7f2556b8f6d27b3f43ea1ab6
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-08-19 13:29:25 +0200

    README, Mention

---
+++ README.md
@@ -0,0 +1,8 @@
+# Redirecty
+
+- Domena: https://krivebot.xyz
+
+1. /twitter 
+2. /discord 
+3. /invite
+4. /github
--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,13 +1,29 @@
 const r = require("rethinkdb")
+const { MessageEmbed } = require("discord.js")
 module.exports = {
     name: "messageCreate",
     once: false,
 
-   async execute(client, message) {
+    async execute(client, message) {
         const { prefix } = require("../config.json")
 
         const args = message.content.slice(prefix.length).trim().split(/ +/);
         const command = args.shift().toLowerCase();
+
+        const WelcomeMessagesArray = ["Witaj", "Miło mi cię poznać!", "Dzień dobry!"]
+        const CommandsMessagesArray = ["ping", "help", "ban", "kick", "set", "logs"]
+
+        const mention = new MessageEmbed()
+            .setTitle(WelcomeMessagesArray.random())
+            .setDescription(`\`\`\`Ping: ${client.ws.ping}\nLiczba serwerów: ${client.guilds.cache.size}\nLiczba użytkowników:\n${client.users.cache.size}\`\`\``)
+            .setFooter(`Polecane komendy: ${CommandsMessagesArray.random()}`)
+            .setColor("ORANGE")
+        const prefixMention = new RegExp(`^<@!?${client.user.id}>( |)$`);
+        if (message.content.match(prefixMention)) {
+            return message.channel.send({ embeds: [mention] }).then(m => {
+                setTimeout(() => m.delete(), 10000);
+            })
+        }
 
         if (message.author.bot) return;
 
@@ -17,7 +33,7 @@ module.exports = {
         if (gban) return client.sender(message, "Otrzymałeś blokadę!", "Nie możesz korzystać z komend!", "", "RED", "", "", "")
 
         const cmd = client.commands.get(command) || client.commands.find(c => c.help.aliases && c.help.aliases == command);
-       
+
         if (cmd) cmd.run(client, message, args)
     }
 }
