3e15d56ab940df018bf272a870b70655dc133a48
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-08-23 01:23:23 +0200

    Suggestions

--- src/commands/tools/set.js
+++ src/commands/tools/set.js
@@ -120,6 +120,20 @@ exports.run = async (client, message, args) => {
 
             client.mentionSender(message, "Ustawiono!", `Zmienna: \`goodbyeChannel\`\nWartość: <#${goodbyeChannel.id}>`, "", "#2003fc", "")
             break;
+        case "suggestChannel":
+            let suggestChannel = message.guild.channels.cache.find(c => c.name.toLowerCase().includes(args[1])) || message.guild.channels.cache.get(args[1]) || message.mentions.channels.first()
+
+            if (!suggestChannel) return client.sender(message, "Błąd!", "Nie znaleziono kanału bądź w ogóle go nie podałeś!", "", "RED", "", "")
+
+            if (suggestChannel.type === "GUILD_VOICE") return client.mentionSender(message, "Błąd!", "Podałeś kanał głosowy! Musisz podać kanał tekstowy.", "", "RED", "")
+            if (suggestChannel.type === "GUILD_CATEGORY") return client.mentionSender(message, "Błąd!", "Podałeś kategorię! Podaj kanał tekstowy.", "", "RED")
+
+            await r.table("settings").insert({ id: message.guild.id, suggestChannel: suggestChannel.id, }).run(client.con)
+
+            await r.table("settings").get(message.guild.id).update({ suggestChannel: suggestChannel.id }).run(client.con)
+
+            client.mentionSender(message, "Ustawiono!", `Zmienna: \`suggestChannel\`\nWartość: <#${suggestChannel.id}>`, "", "#2003fc", "")
+            break;
         case "verifyRole":
             let verifyRole = message.guild.roles.cache.get(args[0]) || message.mentions.roles.first()
             if (!verifyRole) return client.sender(message, "Błąd!", "Nie znaleziono roli bądź w ogóle jej nie podałeś!", "", "RED", "", "")
