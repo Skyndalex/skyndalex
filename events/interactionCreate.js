ab5e6197d5e57bea9221f635dba1c0daa4734819
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-08-27 18:03:45 +0200

    channel.send

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -5,13 +5,13 @@ module.exports = {
     once: false,
 
     async execute(client, interaction) {
-        if (cooldown.has(interaction.user.id)) {
-            interaction.reply({ content: "Poczekaj minutę przed otwarciem następnego ticketa. ", ephemeral: true })
-        } else {
             const table = await r.table("settings").get(interaction.guild.id).run(client.con)
             if (!table?.moderatorRole) return interaction.reply({content: "Administrator serwera nie ustawił roli moderatora!", ephemeral: true})
             
             if (interaction.customId === "createticket") {
+                if (cooldown.has(interaction.user.id)) {
+                    interaction.reply({ content: "Poczekaj minutę przed otwarciem następnego ticketa. ", ephemeral: true })
+                } else {
                 const channel = await interaction.guild.channels.create(`ticket-${interaction.user.tag}`, {
                     type: "GUILD_TEXT",
                     permissionOverwrites: [
@@ -29,12 +29,20 @@ module.exports = {
                         }
                     ]
                 })
+                channel.send({content: `<@${interaction.user.id}>, Pomyślnie otworzyłeś swój ticket!`})
                 await interaction.reply({ content: `Otworzono kanał! (<#${channel.id}>)`, ephemeral: true })
             }
+            cooldown.add(interaction.user.id);
+            setTimeout(() => {
+                cooldown.delete(interaction.user.id);
+            }, 60000);
         }
-        cooldown.add(interaction.user.id);
-        setTimeout(() => {
-            cooldown.delete(interaction.user.id);
-        }, 60000);
+        if (interaction.customId === "ticketConfirm") {
+            if (!interaction.member.permissions.has('MANAGE_CHANNELS')) return interaction.reply({content: "Nie masz permisji", ephemeral: true})
+
+            await r.table("tickets").insert({ id: interaction.guild.id, activate: true }).run(client.con)
+
+            interaction.reply({content: "Pomyślnie włączono tickety", ephemeral: true})
+        }
     }
 }
