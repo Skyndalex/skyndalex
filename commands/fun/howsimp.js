756d81628881350393fe0ba9fa225045d9756b0e
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-09-04 15:08:21 +0200

    unx fajen

--- commands/fun/howsimp.js
+++ commands/fun/howsimp.js
@@ -1,6 +1,8 @@
 exports.run = async (client, message, args) => {
-    if (args[0] === "kubus") return message.channel.send({content: "no kubus na 100% simpuje do tomka, tego nawet sprawdzac nie musze."})
-    if (args[0] === "tomek") return message.channel.send({content: "no tomek to do ciaha akurat"})
+    if (args[0] === "kubus") return message.reply("no kubus na 100% simpuje do tomka, tego nawet sprawdzac nie musze")
+    if (args[0] === "tomek") return message.reply("no tomek to do ciaha akurat")
+    if (args[0] === "unx") return message.reply("kto to jest?")
+
     let percent = Math.floor(Math.random() * (100 - 0) + 0)
 
     if (!args[0]) return client.sender(message, "401: No content.", "Nie podano argumentów", client.footer, "RED", "", "")
