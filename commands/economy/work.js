40ffb202d626ffed2a358816c9fa421d4c5aa7d4
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-09-10 20:46:49 +0200

    + 0

--- commands/economy/work.js
+++ commands/economy/work.js
@@ -4,14 +4,14 @@ exports.run = async (client, message) => {
     if (!table?.job) return client.sender(message, "", "**Błąd**\n\nNie dołączyłeś do żadnej pracy!", "Ekonomia", "RED")
     switch (table?.job) {
         case "developer":
-            let developerMoney = (Math.floor(Math.random() * (150 - 0) + 0))
+            let developerMoney = (Math.floor(Math.random() * (150 - 0)))
 
             client.sender(message, ``, `Twoja praca: **Developer**\n\nOtrzymano łącznie ${developerMoney} monet!`, `Ekonomia`, `GREEN`, ``, ``, ``)
 
             await r.table("economy").update({ money: table.money + developerMoney }).run(client.con)
             break;
         case "miner":
-            let minerMoney = (Math.floor(Math.random() * (100 - 0) + 0))
+            let minerMoney = (Math.floor(Math.random() * (100 - 0)))
 
             client.sender(message, ``, `Twoja praca: **Górnik**\n\nOtrzymano łącznie ${minerMoney} monet!`, `Ekonomia`, `GREEN`, ``, ``, ``)
 
