c4f69ac035c0296f1aea1215da817e792cf676e4
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-09-11 14:07:51 +0200

    ping

--- commands/bot/ping.js
+++ commands/bot/ping.js
@@ -1,5 +1,5 @@
 exports.run = (client, message) => {
-    client.sender(message, "", `Ping: \`${client.ws.ping}ms\`\n\nDokumentacja: https://docs.krivebot.xyz\nStatus serwisów: https://status.krivebot.xyz\nSerwer wsparcia: ${client.discord}`, "", "YELLOW", "", "", "")
+    client.sender(message, "", `Ping: \`${client.ws.ping}ms\`\n\nDokumentacja: https://docs.krivebot.xyz\nStatus serwisów: https://status.krivebot.xyz\nSerwer wsparcia: https://${client.discord}`, "", "YELLOW", "", "", "")
 }
 exports.help = {
     name: "ping",
