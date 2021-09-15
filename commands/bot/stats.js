9cc0075dc0d4447570be54ff14ebe493c2c48179
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-09-15 21:49:23 +0200

    freeram.com

--- commands/bot/stats.js
+++ commands/bot/stats.js
@@ -1,15 +1,18 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require("discord.js")
-const wait = require('util').promisify(setTimeout);
-
+const { MessageEmbed } = require("discord.js");
+const os = require("os");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('stats')
         .setDescription('Statystyki bota'),
 
     async execute(client, interaction) {
+        let rss = `${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)}/${(os.totalmem() / 1024 / 1024).toFixed(2)} MB`
+        let heapTotal = `${(process.memoryUsage().heapTotal / 1024 / 1024).toFixed(2)}/${(os.totalmem() / 1024 / 1024).toFixed(2)} MB`
+
         const embed = new MessageEmbed()
             .addField(`\`Cache:\``, `Serwery: ${client.guilds.cache.size}\nUżytkownicy: ${client.users.cache.size}\nKanały: ${client.channels.cache.size}\nEmoji: ${client.emojis.cache.size}`)
+            .addField(`\`Zasoby: (rss/heapTotal)\``, `${rss} / ${heapTotal}`)
             .setColor("GREEN")
         interaction.reply({embeds: [embed]})
     }
