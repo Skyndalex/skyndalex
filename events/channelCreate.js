4c368afeb79635c6792f30064bf7f33663945ebc
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-09-18 21:08:29 +0200

    logs [test from v4.0 branch]

---
+++ events/channelCreate.js
@@ -0,0 +1,24 @@
+const r = require("rethinkdb")
+const { MessageEmbed } = require("discord.js")
+module.exports = {
+    name: "channelCreate",
+    once: false,
+
+    async execute(client, channel) {
+        const channelLog = await r.table("logs").get(channel.guild.id)("channelCreate").run(client.con).catch(err => { })
+
+        if (channel.type === "GUILD_CATEGORY") {
+            const logEmbed = new MessageEmbed()
+                .setDescription(`**Utworzono kategorię!**\n\nNazwa: ${channel.name || "Brak"}\nID: ${channel.id || "Brak" }\nPozycja: ${channel.rawPosition || "Brak" }`)
+                .setTimestamp()
+                .setColor("YELLOW")
+            channel.guild.channels.cache.get(channelLog).send({ embeds: [logEmbed] }).catch(err => {})
+        } else {
+            const logEmbed2 = new MessageEmbed()
+                .setDescription(`**Utworzono kanał!**\n\nNazwa: ${channel.name}\nID: ${channel.id}\nPozycja: ${channel.rawPosition}\nBitrate: ${channel.bitrate || "Brak"}`)
+                .setTimestamp()
+                .setColor("YELLOW")
+            channel.guild.channels.cache.get(channelLog).send({ embeds: [logEmbed2] }).catch(err => {})
+        }
+    }
+}
---
+++ events/channelUpdate.js
@@ -0,0 +1,24 @@
+const r = require("rethinkdb")
+const { MessageEmbed } = require("discord.js")
+module.exports = {
+    name: "channelUpdate",
+    once: false,
+
+    async execute(client, oldChannel, newChannel) { 
+        const channelLog = await r.table("logs").get(oldChannel.guild.id)("channelUpdate").run(client.con).catch(err => {})
+
+        if (oldChannel.type === "GUILD_CATEGORY") {
+            const logEmbed = new MessageEmbed()
+            .setDescription(`**Zaktualizowano kategorię!**\n\nNazwa przed: ${oldChannel.name || "Brak" }\nID: ${oldChannel.id}\nPozycja przed: ${oldChannel.rawPosition || "Brak" }\nPozycja po: ${newChannel.rawPosition || "Brak" }`)
+            .setTimestamp()
+            .setColor("YELLOW")
+            oldChannel.guild.channels.cache.get(channelLog).send({embeds: [logEmbed]}).catch(err => {})
+        } else {
+            const logEmbed2 = new MessageEmbed()
+            .setDescription(`**Zaktualizowano kanał!**\n\nNazwa przed: ${oldChannel.name}\nNazwa po: ${newChannel.name}\nID: ${oldChannel.id}\nPozycja przed: ${oldChannel.rawPosition}\nPozycja po: ${newChannel.rawPosition}`)
+            .setTimestamp()
+            .setColor("YELLOW")
+            oldChannel.guild.channels.cache.get(channelLog).send({embeds: [logEmbed2]}).catch(err => {})
+        }
+    }
+}
