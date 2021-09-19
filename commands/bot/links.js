484ddf2ffe180bc5e99c2070623a2180517e50fb
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-09-19 21:56:43 +0200

    links update

--- commands/bot/links.js
+++ commands/bot/links.js
@@ -7,6 +7,6 @@ module.exports = {
         .setDescription('Linki'),
 
     async execute(client, interaction) {
-        client.builder(interaction, "", "**Linki**\n\n[\`Discord\`](https://krivebot.xyz/dicord)\n[\`GitHub\`](https://krivebot.xyz/github)\n[\`Invite\`](https://krivebot.xyz/invite)\n[\`Twitter\`](https://krivebot.xyz/twitter)\n[\`GH Projects\`](https://krivebot.xyz/todo)", `Linki`, `GREEN`, ``)
+        client.builder(interaction, "", "**Linki**\n\n[\`Discord\`](https://krivebot.xyz/dicord)\n[\`GitHub\`](https://krivebot.xyz/github)\n[\`Invite\`](https://krivebot.xyz/invite)\n[\`Twitter\`](https://krivebot.xyz/twitter)\n[\`GH Projects\`](https://krivebot.xyz/todo)\n[\`Dokumentacja\`](https://docs.krivebot)\n[\`Status\`](https://status.krivebot.xyz)", `Linki`, `GREEN`, ``)
     }
 };
