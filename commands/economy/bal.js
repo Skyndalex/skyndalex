5a4980bf03d771823f6a0bf0f8d98226ed772ca8
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-09-19 21:56:23 +0200

    bal

---
+++ commands/economy/bal.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const r = require("rethinkdb")
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('bal')
+        .setDescription('Sprawdź swój stan konta.'),
+    async execute(client, interaction) {
+        const table = await r.table("economy").get(interaction.user.id).run(client.con)
+        if (!table?.job) return interaction.reply({content: "Nie jesteś w pracy!"})
+
+        let works = {
+            miner: "Górnik",
+            Developer: "Programista"
+        }
+        client.builder(interaction, ``, `**Stan konta**\n\nTwoja praca: ${works[table.job]}\nIlość monet: ${table.money}\nUżytkownik: ${interaction.user.tag}`, `Ekonomia`, `GREEN`)
+    }
+
+};
