84916bc13ba1cbb8144a11f34f879fe481999e5c
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-09-19 13:03:17 +0200

    verify if

--- commands/tools/verify.js
+++ commands/tools/verify.js
@@ -7,6 +7,9 @@ module.exports = {
         .setDescription('Zweryfikuj się.'),
 
     async execute(client, interaction) {
+        const table = await r.table("settings").get(interaction.guild.id).run(client.con)
+        if (!table?.userRole) return interaction.reply({content: "Nie ustawiono roli zweryfikowanego użytkownika na serwerze!"})
+
         if (!interaction.guild.me.permissions.has("MANAGE_ROLES")) return interaction.reply({content: "Nie mam permisji do nadawania roli!"})
 
         const row = new MessageActionRow()
