781c7eb89bcac3272f9a90eeabd5b855042381c2
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-09-20 16:16:29 +0200

    jeja command

--- commands/fun/jeja.js
+++ commands/fun/jeja.js
@@ -1,11 +1,20 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-
+const fetch = require('node-fetch')
+const { parse } = require('node-html-parser')
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('jeja')
         .setDescription('Generuje śmieszny mem z jeja.pl'),
 
     async execute(client, interaction) {
+        fetch('https://memy.jeja.pl/losowe')
+            .then(res => res.text())
+            .then(body => {
+                const root = parse(body)
+                const img = parse(root.querySelector('.ob-left-image').toString())
+                const mem = img.querySelector('img').getAttribute('src')
 
+                client.builder(interaction, "", "", "Źródło: jeja.pl", "#000000", "", mem)
+            })
     }
 };
