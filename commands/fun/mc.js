9807fdf6110244676c4a7fe5881816e7ce557a4c
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-09-20 16:16:41 +0200

    mc command

--- commands/fun/mc.js
+++ commands/fun/mc.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-
+const fetch = require("node-fetch")
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('mc')
@@ -9,6 +9,34 @@ module.exports = {
         )),
 
     async execute(client, interaction) {
+        const player = await interaction.options.getString("player");
 
+        fetch(`https://api.ashcon.app/mojang/v2/user/${player}`).then(res => res.json())
+            .then(json => {
+                let data = [];
+                for (let i in json.username_history) {
+                    data.push(json.username_history[i].username);
+                }
+                if (!data) return interaction.reply({content: "Nie podałeś nazwy gracza!"});
+
+                client.builder(interaction, `Informacje o koncie minecraft: ${json.username}`, ``, ``, `GREEN`, [
+                    {
+                        name: "Konto stworzono o:",
+                        value: json.created_at||"Nie znaleziono danych: \`created_at\`"
+                    },
+                    {
+                        name: "Nazwa użytkownika",
+                        value: json.username||"Nie znaleziono danych: \`username\`"
+                    },
+                    {
+                        name: "UUID",
+                        value: json.uuid||"Nie znaleziono danych: \`uuid\`"
+                    },
+                    {
+                        name: "Historia nicków (najstarszy --> najnowszy)",
+                        value: `\`\`\`${data.join(" , ")}\`\`\``||"Nie znaleziono danych: \`username_history\`"
+                    }
+                ])
+            })
     }
 };
