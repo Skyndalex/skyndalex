d9246343be7206e517e011d5ef9a6ddc3db04a48
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-09-20 15:55:41 +0200

    new packages

--- commands/fun/kaczkoland.js
+++ commands/fun/kaczkoland.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-
+const fetch = require("node-fetch")
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('kaczkoland')
