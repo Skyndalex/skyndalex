65c24df0937d7ae09cfa6a6ee7cb423c484886cc
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-09-22 20:30:45 +0200

    ktg

--- commands/economy/bal.js
+++ commands/economy/bal.js
@@ -5,14 +5,14 @@ module.exports = {
         .setName('bal')
         .setDescription('Sprawdź swój stan konta.'),
     async execute(client, interaction) {
-        const table = await r.table("economy").get(interaction.user.id).run(client.con)
-        if (!table?.job) return interaction.reply({content: "Nie jesteś w pracy!"})
+        const table = await r.table("economy").get(interaction.user.id).run(client.con);
+        if (!table?.job) return interaction.reply({content: "Nie jesteś w pracy!"});
 
         let works = {
             miner: "Górnik",
             Developer: "Programista"
-        }
+        };
+
         client.builder(interaction, ``, `**Stan konta**\n\nTwoja praca: ${works[table.job] || "Błąd"}\nIlość monet: ${table.money}\nUżytkownik: ${interaction.user.tag}`, `Ekonomia`, `GREEN`)
     }
-
 };
