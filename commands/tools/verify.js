58deff470a55f49105453e49c04479e532bac4b2
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-09-26 20:11:11 +0200

    CaptchaVerification

--- commands/tools/verify.js
+++ commands/tools/verify.js
@@ -1,5 +1,8 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
+const { MessageAttachment } = require('discord.js');
+const path = require("path")
+const Captcha = require("@haileybot/captcha-generator");
+const fs = require("fs")
 const r = require("rethinkdb");
 module.exports = {
     data: new SlashCommandBuilder()
@@ -7,21 +10,24 @@ module.exports = {
         .setDescription('Zweryfikuj się.'),
 
     async execute(client, interaction) {
+        if (!interaction.guild.me.permissions.has("MANAGE_ROLES")) return interaction.reply({content: "Nie mam permisji do nadawania roli!"})
+
         const table = await r.table("settings").get(interaction.guild.id).run(client.con)
         if (!table?.userRole) return interaction.reply({content: "Nie ustawiono roli zweryfikowanego użytkownika na serwerze!"})
 
-        if (!interaction.guild.me.permissions.has("MANAGE_ROLES")) return interaction.reply({content: "Nie mam permisji do nadawania roli!"})
+        let captcha = new Captcha();
 
-        const row = new MessageActionRow()
-            .addComponents(
-                new MessageButton()
-                    .setCustomId('verify')
-                    .setLabel('Zweryfikuj się')
-                    .setStyle('SUCCESS'),
-            );
-        interaction.reply({
-            content: "Zweryfikuj się.",
-            components: [row],
+        await interaction.reply({content: "Wpisz captchę poniżej.", files: [new MessageAttachment(captcha.JPEGStream, "captcha.jpeg")]})
+
+        let collector = interaction.channel.createMessageCollector(m => m.author.id === interaction.user.id);
+        collector.on("collect", m => {
+            if (m.content.toUpperCase() === captcha.value) {
+                interaction.editReply({content: "Zweryfikowano."})
+                collector.stop()
+            } else {
+                interaction.editReply({content: "Coś poszło nie tak! Najprawdopodobniej podałeś zły kod. Pamiętaj o dużych znakach itp!", files: null})
+                collector.stop()
+            }
         })
     }
 };
