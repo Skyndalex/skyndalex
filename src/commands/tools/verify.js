1cd190507eef646c5faed4eb9483c22e41210a4e
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-10-14 23:02:03 +0200

    Verify fixes

--- src/commands/tools/verify.js
+++ src/commands/tools/verify.js
@@ -10,10 +10,10 @@ module.exports = {
         .setDescription('Zweryfikuj się.'),
 
     async execute(client, interaction) {
-        if (!interaction.guild.me.permissions.has("MANAGE_ROLES")) return interaction.reply({content: "Nie mam permisji do nadawania roli!"})
-
         const table = await r.table("settings").get(interaction.guild.id).run(client.con)
-        if (!table?.userRole) return interaction.reply({content: "Nie ustawiono roli zweryfikowanego użytkownika na serwerze! \`(userRole)\`"})
+        if (!table.userRole) return interaction.reply({content: "Nie ustawiono roli zweryfikowanego użytkownika na serwerze! \`(userRole)\`"})
+
+        if (!interaction.guild.me.permissions.has("MANAGE_ROLES")) return interaction.reply({content: "Nie mam permisji do nadawania roli!"})
 
         if (interaction.member.roles.cache.map(r => r.id).includes(table?.userRole)) return client.builder(interaction, "", "**Błąd!**\n\nJesteś już zweryfikowany! Nie możesz zweryfikować się po raz drugi.", "", "RED", "", "")
 
@@ -21,7 +21,9 @@ module.exports = {
 
         await interaction.reply({content: "Wpisz captchę poniżej.", files: [new MessageAttachment(captcha.JPEGStream, "captcha.jpeg")]})
 
-        let collector = interaction.channel.createMessageCollector(m => m.author.id === interaction.user.id);
+        const filter = m => m.author.id === interaction.user.id;
+
+        let collector = interaction.channel.createMessageCollector({filter, time: 15000});
         collector.on("collect", m => {
             if (m.content.toUpperCase() === captcha.value) {
                 interaction.member.roles.add(table.userRole)
