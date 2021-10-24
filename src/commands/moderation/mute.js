38db2068dab17401730a00de184ea624f30a2f1b
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-10-24 21:23:30 +0200

    mute fix

--- src/commands/moderation/mute.js
+++ src/commands/moderation/mute.js
@@ -18,12 +18,8 @@ module.exports = {
         const settings = await r.table("settings").get(interaction.guild.id).run(client.con)
         if (!settings?.mutedRole) interaction.reply({content: "Administrator serwera nie ustawił roli wyciszonego!"})
 
-        await r.table("users").get(interaction.user.id).update({ isMuted: true }).run(client.con);
-
         const member = interaction.options.getMember("user");
         const reason = interaction.options.getString("reason");
-
-        const roleWatcher = await r.table("users").insert({ userid: member.user.id, isMuted: true, }).run(client.con);
 
         member.roles.add(settings?.mutedRole, { reason: "Wyciszono." })
 
