ab428eb5d663773199e93b3227dc436f1b578e38
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-10-24 21:23:12 +0200

    guildMemberRemove channel fix

--- src/events/guildMemberRemove.js
+++ src/events/guildMemberRemove.js
@@ -5,7 +5,8 @@ module.exports = {
     once: false,
 
     async execute(client, member) {
-        const table = await r.table("settings").get(member.guild.id).run(client.con).catch(err => {false})
+        const table = await r.table("settings").get(member.guild.id).run(client.con)
+        if (!table?.goodbyeChannel) return;
 
         const embed = new MessageEmbed()
             .setDescription(`**Właśnie ktoś wyszedł!**\n\nWitamy użytkownika ${member.user}(${member.user.tag})! Mamy nadzieję że się będziesz dobrze u nas bawił.\nAktualna ilość osób na serwerze: ${member.guild.memberCount}`)
