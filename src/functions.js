9826e8537f2f290b87f5b80170cd3128b9d821d4
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-10-29 21:21:12 +0200

    Stable branch configuration.

--- src/functions.js
+++
@@ -1,8 +0,0 @@
-const r = require("rethinkdb")
-module.exports = async (client) => {
-    Object.defineProperty(Array.prototype, "random", {
-        value: function () {
-            return this[Math.floor(Math.random() * this.length)];
-        }
-    });
-}
