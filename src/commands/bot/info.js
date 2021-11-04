5f35a9676905ac6b89b99228c97301f2d2a976c6
Remixiak <63855221+ThatRemixiak@users.noreply.github.com>
2021-11-04 22:08:42 +0100

    Im gud england spiker

--- src/commands/bot/info.js
+++ src/commands/bot/info.js
@@ -5,8 +5,8 @@ module.exports = {
     run: async (client, interaction) => {
         client.builder(interaction, `Bot informations`, ``, ``, `GREEN`, [
             { name: "> Important links", value: "[\`• Statuspage\`](https://status.krivebot.xyz)\n[\`• Documentation\`](https://docs.krivebot.xyz)\n[\`• Discord server\`](https://discord.gg/WEas4WFjse)\n[\`• Github organization\`](https://github.com/KriveWasTaken)"},
-            { name: "> History", value: "We are started as *Korrumix* in 2018 which was created in DBM so it was never public. Only later did I start writing bots in JavaScript (2018 end). We want to start writing more projects in js, linked to Krive."},
-            { name: "> Why this bot is open-source?", value: "We want everyone to have access to the code. That way everyone can check it out, report major bugs with optimilization, and more."},
+            { name: "> History", value: "We started as **Korrumix** in 2018 which was created in DBM but never released to public. In late 2018 we started writing bots in JavaScript, as we progress we want to write more project in js, linked to Krive, but we don't have enough experience yet."},
+            { name: "> Why this bot is open-source?", value: "We want still to progress so we published code publicly so you as a user can check it out and report bugs or help with optimization."},
             { name: "> What is open-source?", value: "[\`• Documentation\`](https://github.com/KriveWasTaken/docs-public)\n[\`• Manager\`](https://github.com/KriveWasTaken/manager)\n[\`• General bot\`](https://github.com/KriveWasTaken/krivebot)"},
             { name: `> Stats`, value: `• Servers: ${client.guilds.cache.size}\n• Users: ${client.users.cache.size}\n• Channels: ${client.channels.cache.size}\n• Emojis: ${client.emojis.cache.size}`}
         ]).toString(); // embed string value
