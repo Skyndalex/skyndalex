a1971fadbb3d20ac24484d0b2655fbd57a8d7400
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-11-09 20:08:39 +0100

    base embed builder fix

--- src/base.js
+++ src/base.js
@@ -10,13 +10,13 @@ class Base extends Client {
 
     builder (interaction, title, text, footer, color, fields = [], image, ephemeral = false) {
         const embed = new MessageEmbed()
-            .setTimestamp();
-        if (title) embed.setTitle(title);
-        if (text) embed.setDescription(text);
-        if (color) embed.setColor(color);
-        if (footer) embed.setFooter(footer);
-        if (image) embed.setImage(image);
-        if (fields.length) embed.addFields(fields);
+            .setTimestamp()
+            .setTitle(title)
+            .setDescription(text)
+            .setColor(color)
+            .setFooter(footer)
+            .setImage(image)
+            .addFields(fields)
         return interaction.reply({ embeds: [embed], allowedMentions: {parse: []}, ephemeral: ephemeral});
     }
 }
