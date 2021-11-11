1e6ccc472484fcd499adba5a6c3444450aa98d62
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-11-11 20:28:27 +0100

    no i po co to zrobiłem...

--- src/commands/tools/comments.js
+++ src/commands/tools/comments.js
@@ -2,18 +2,10 @@ module.exports = {
     name: "comments",
     description: "Comments system.",
     options: [
-        { name: "enable", description: "Enable comments", type: 3, choices: [
-                { name: "suggestions", value: "suggestions_comments"},
-                { name: "votings", value: "votings_comments"},
-                { name: "announcements", value: "broadcasts_comments"}
-            ]},
-        { name: "disable", description: "Disable comments", type: 3, choices: [
-                { name: "suggestions", value: "suggestions_comments_disable"},
-                { name: "votings", value: "votings_comments_disable"},
-                { name: "announcements", value: "broadcasts_comments_disable"}
-            ]},
-    ],
+        { type: "STRING", name: "enable", description: "enable comments", choices: [ { name: "Suggestions comment system", value: "Enable"}]},
+        { type: "STRING", name: "disable", description: "disable comments", choices: [ { name: "Suggestions comment system", value: "Disable"}]}],
 
     run: async (client, interaction) => {
+
     }
 }
