9235932847a5b8830419ae5deb1e039c59094bd4
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-11-16 16:37:37 +0100

    Settings - Introducing

--- src/commands/tools/set.js
+++ src/commands/tools/set.js
@@ -1,13 +1,23 @@
+const r = require("rethinkdb")
 module.exports = {
     name: "set",
     description: "Bot settings.",
     options: [
-        { type: "CHANNEL", name: "suggestions", description: "Suggestions channel", channelTypes: ["GUILD_TEXT"]}
+        { type: "CHANNEL", name: "broadcast", description: "Broadcast channel", channelTypes: ["GUILD_TEXT"]},
+        { type: "CHANNEL", name: "suggestions", description: "Suggestions channel", channelTypes: ["GUILD_TEXT"]},
+        { type: "CHANNEL", name: "complaints", description: "Complaints channel", channelTypes: ["GUILD_TEXT"]},
+        { type: "CHANNEL", name: "images", description: "Images channel", channelTypes: ["GUILD_TEXT"]},
+        { type: "CHANNEL", name: "welcome_channel", description: "Welcome channel", channelTypes: ["GUILD_TEXT"]},
+        { type: "CHANNEL", name: "goodbye_channel", description: "Goodbye channel", channelTypes: ["GUILD_TEXT"]},
+        { type: "CHANNEL", name: "applications", description: "Applications channel", channelTypes: ["GUILD_TEXT"]},
+        { type: "CHANNEL", name: "moderator_role", description: "Server moderator role [Required for tickets]", channelTypes: ["GUILD_TEXT"]},
+        { type: "CHANNEL", name: "muted_role", description: "The role of the muted user"},
+        { type: "CHANNEL", name: "user_role", description: "User role [Required for verification]", channelTypes: ["GUILD_TYPES"]},
+        { type: "CHANNEL", name: "auto_role", description: "If a user enters the server, they will automatically get the role set.", channelTypes: ["GUILD_TYPES"]},
+
     ],
 
     run: async (client, interaction) => {
-        client.builder(interaction, `Settings`, ``, `Logs settings: /logs`, `GREEN`, [
-            { name: "\`suggestionsChannel\`", value: `> Suggestion channel.\n*psst, you can set a comment system. \`/comments\`*`}
-        ])
+        // code
     }
 }
