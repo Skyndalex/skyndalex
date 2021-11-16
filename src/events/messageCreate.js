ef341e3df0e4ed570d8872006c662342e57d3101
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-11-16 19:45:22 +0100

    omg eval!!

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -14,5 +14,40 @@ module.exports = {
         if (message.content.match(prefixMention)) {
             return message.channel.send({embeds: [mention]})
         }
+
+        const prefix = "s."
+
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
+        const command = args.shift().toLowerCase();
+
+        if (command === "eval") {
+             let prodef = ["817883855310684180"];
+             if (!prodef.includes(message.author.id)) return message.reply({content: "Only the bot owner can use this command"});
+             if (!args) return message.channel.send({content: "Enter code"})
+
+             let cToken = new RegExp(client.token, "g");
+
+            const clean = text => {
+                if (typeof text === "string") {
+                    return text
+                        .replace(/`/g, "`" + String.fromCharCode(8203))
+                        .replace(/@/g, "@" + String.fromCharCode(8203))
+                        .replace(cToken, "g");
+                } else {
+                    return text;
+                }
+            };
+
+            try {
+                const code = args.join(" ")
+                let evaled = eval(code);
+
+                if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
+                message.channel.send({content: `\`\`\`js\n${clean(evaled)}\`\`\``});
+
+            } catch (err) {
+                message.channel.send({content: `\`\`\`js\n${clean(err)}\n\`\`\``});
+            }
+        }
     }
 }
