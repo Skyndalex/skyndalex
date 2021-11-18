1a2dd99ac78b6aab55dc24269657388d6b21c928
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-11-18 19:39:32 +0100

    broadcastChannel

--- src/commands/tools/set.js
+++ src/commands/tools/set.js
@@ -1,15 +1,16 @@
 const r = require("rethinkdb")
+const {MessageEmbed} = require("discord.js");
 module.exports = {
     name: "set",
     description: "Bot settings.",
     options: [
-        { type: "CHANNEL", name: "broadcast", description: "Broadcast channel", channelTypes: ["GUILD_TEXT"]},
-        { type: "CHANNEL", name: "suggestions", description: "Suggestions channel", channelTypes: ["GUILD_TEXT"]},
-        { type: "CHANNEL", name: "complaints", description: "Complaints channel", channelTypes: ["GUILD_TEXT"]},
-        { type: "CHANNEL", name: "images", description: "Images channel", channelTypes: ["GUILD_TEXT"]},
+        { type: "CHANNEL", name: "broadcast_channel", description: "Broadcast channel", channelTypes: ["GUILD_TEXT"]},
+        { type: "CHANNEL", name: "suggestions_channel", description: "Suggestions channel", channelTypes: ["GUILD_TEXT"]},
+        { type: "CHANNEL", name: "complaints_channel", description: "Complaints channel", channelTypes: ["GUILD_TEXT"]},
+        { type: "CHANNEL", name: "images_channel", description: "Images channel", channelTypes: ["GUILD_TEXT"]},
         { type: "CHANNEL", name: "welcome_channel", description: "Welcome channel", channelTypes: ["GUILD_TEXT"]},
         { type: "CHANNEL", name: "goodbye_channel", description: "Goodbye channel", channelTypes: ["GUILD_TEXT"]},
-        { type: "CHANNEL", name: "applications", description: "Applications channel", channelTypes: ["GUILD_TEXT"]},
+        { type: "CHANNEL", name: "applications_channel", description: "Applications channel", channelTypes: ["GUILD_TEXT"]},
         { type: "CHANNEL", name: "moderator_role", description: "Server moderator role [Required for tickets]", channelTypes: ["GUILD_TEXT"]},
         { type: "CHANNEL", name: "muted_role", description: "The role of the muted user"},
         { type: "CHANNEL", name: "user_role", description: "User role [Required for verification]", channelTypes: ["GUILD_TEXT"]},
@@ -20,7 +21,25 @@ module.exports = {
             for (let option of interaction.options.data) {
                 switch (option.name) {
                     case "broadcast":
-                        interaction.reply({content: `Choosed option: ${option.name}`}) // Test message
+                        /*
+                        The default builder for embeds is added here temporarily.
+                        I will change it if I rewrite the current one from the base.js file (in root folder)
+                         */
+
+                        if (!interaction.member.permissions.has('MANAGE_CHANNELS')) return interaction.reply({content: "You have no permissions: \`MANAGE_CHANNELS'`", ephemeral: true});
+                        if (!interaction.guild.me.permissions.has("MANAGE_CHANNELS")) return interaction.reply({content: "I have no permissions: \`MANAGE_CHANNELS\`"});
+
+                        const channel = await interaction.options.getChannel("broadcast");
+
+                        await r.table("settings").insert({ id: interaction.guild.id, broadcastChannel: channel.id}).run(client.con);
+                        await r.table("settings").get(interaction.guild.id).update({ id: interaction.guild.id, broadcastChannel: channel.id }).run(client.con);
+
+                        const embedBroadcast = new MessageEmbed()
+                            .setTitle("Successfully set!")
+                            .setDescription(`Selected option: \`${option.name}\``)
+                            .addField(`New value`, `<#${channel.id}>`)
+                            .setColor("DARK_BUT_NOT_BLACK")
+                        interaction.reply({ embeds: [embedBroadcast] })
                         break;
                 }
             }
