037464501bb7bd2cef00a3bff61c42a227fa5ada
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-11-23 16:54:00 +0100

    Channel types (settings

--- src/commands/tools/set.js
+++ src/commands/tools/set.js
@@ -1,30 +1,34 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require("discord.js")
+const { MessageEmbed } = require("discord.js");
 const r = require("rethinkdb")
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("set")
-        .setDescription("Settings")
-        .addChannelOption(option =>
-            option.setName("broadcast-channel").setDescription("Broadcast channel."))
-        .addChannelOption(option =>
-            option.setName("suggestions-channel").setDescription("Suggestions channel"))
-        .addChannelOption(option =>
-            option.setName("vote-channel").setDescription("Voting channel"))
-        .addChannelOption(option =>
-            option.setName("complaint-channel").setDescription("Complaint channel"))
-        .addChannelOption(option =>
-            option.setName("image-channel").setDescription("Images channel"))
-        .addChannelOption(option =>
-            option.setName("application-channel").setDescription("Applications channel")),
+    data: {
+        name: "set",
+        description: "Bot settings.",
+        options: [
+            { type: 7, name: "broadcast-channel", description: "Broadcast channel" },
+            { type: 7, name: "suggestions-channel", description: "Suggestions channel" },
+            { type: 7, name: "complaints-channel", description: "Complaints channel" },
+            { type: 7, name: "images-channel", description: "Images channel" },
+            { type: 7, name: "welcome-channel", description: "Welcome channel" },
+            { type: 7, name: "goodbye-channel", description: "Goodbye channel" },
+            { type: 7, name: "applications-channel", description: "Applications channel" },
+            { type: 8, name: "moderator-role", description: "Server moderator role [Required for tickets]" },
+            { type: 8, name: "muted-role", description: "The role of the muted user" },
+            { type: 8, name: "user-role", description: "User role [Required for verification]" },
+            { type: 8, name: "auto-role", description: "If a user enters the server, they will automatically get the role set." },
+    ],
+},
+
     async execute(client, interaction) {
         if (!interaction.member.permissions.has('MANAGE_CHANNELS')) return interaction.reply({content: "You need permissions: \`MANAGE_CHANNELS\`"});
 
         for (let option of interaction.options.data) {
             switch (option.name) {
-                // Algorithms needs rewrite. I know it.
                 case "broadcast-channel":
-                    const channel = await interaction.options.getChannel("broadcast_channel");
+                    const channel = await interaction.options.getChannel("broadcast-channel");
+
+                    if (channel.type === "GUILD_CATEGORY") return interaction.reply({content: "\`Specify a text channel.\`"});
+                    if (channel_suggestions.type === "GUILD_VOICE") return interaction.reply({content: "\`Specify a text channel.\`"});
 
                     await r.table("settings").insert({ id: interaction.guild.id, broadcastChannel: channel.id }).run(client.con)
                     await r.table("settings").get(interaction.guild.id).update({ broadcastChannel: channel.id }).run(client.con)
@@ -38,6 +42,9 @@ module.exports = {
                     break;
                 case "suggestions-channel":
                     const channel_suggestions = await interaction.options.getChannel("suggestions-channel");
+
+                    if (channel_suggestions.type === "GUILD_CATEGORY") return interaction.reply({content: "\`Specify a text channel.\`"});
+                    if (channel.type === "GUILD_VOICE") return interaction.reply({content: "\`Specify a text channel.\`"});
 
                     await r.table("settings").insert({ id: interaction.guild.id, suggestionsChannel: channel_suggestions.id }).run(client.con)
                     await r.table("settings").get(interaction.guild.id).update({ suggestionsChannel: channel_suggestions.id }).run(client.con)
