fda36b533a506b137cd401b1e0cded861783c00b
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-11-23 17:47:55 +0100

    ContextMenu handling

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -14,6 +14,12 @@ module.exports = {
         try {
             await command.execute(client, interaction);
 
+            if (interaction.isContextMenu()) {
+                await interaction.deferReply({ ephemeral: false });
+                const command = client.slashCommands.get(interaction.commandName);
+                if (command) command.run(client, interaction);
+            }
+
         } catch (error) {
             if (error) console.error(error);
             await interaction.reply({ content: 'An error occurred while running the command! An error was sent to the console\nReason: The message may be too long, or there was some other problem with the code. Either way, I have contacted the administration'});
