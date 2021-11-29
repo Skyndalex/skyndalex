7e80377bd8a8ff3f468bba23a55daf2b5a3b09fc
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-11-29 17:32:29 +0100

    guildMemberAdd.js

---
+++ src/events/guildMemberAdd.js
@@ -0,0 +1,17 @@
+const r = require("rethinkdb")
+const { MessageEmbed } = require("discord.js");
+module.exports = {
+    name: "guildMemberAdd",
+    once: false,
+
+    async execute(client, member) {
+        const table = await r.table("settings").get(member.guild.id).run(client.con);
+        if (!table?.welcomeChannel) return;
+
+        let embed = new MessageEmbed()
+            .setTitle(`User joined!`)
+            .setDescription(`Welcome ${member.user.tag}! there are ${member.guild.memberCount} people on the server `)
+            .setColor("GREEN")
+        await member.guild.channels.cache.get(table.welcomeChannel).send({ embeds: [embed]})
+    }
+}
