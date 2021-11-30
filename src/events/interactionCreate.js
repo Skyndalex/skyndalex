4d2cd4448769f84d9abdb9592ac8d516b07f4c91
Korrumz2 <55253305+Korrumz2PL@users.noreply.github.com>
2021-11-30 15:38:19 +0100

    super cool update

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -31,7 +31,7 @@ module.exports = {
                 .setDescription(`Server ID: ${interaction.guild.id} (${interaction.guild.name})\nError:\n\`\`\`${error || "None"}\`\`\``)
                 .setColor("DARK_BUT_NOT_BLACK")
                 .setTimestamp()
-            if (error) client.channels.cache.get("914250038744604672").send({embeds: [errorEmbedChannel]})
+            if (error) client.channels.cache.get("914250038744604672").send({ embeds: [errorEmbedChannel] })
 
             let errorEmbed = new MessageEmbed()
                 .setDescription(`An error has occurred! View [Documentation](https://docs.krivebot.xyz)\nOr [Required bot permissions](https://docs.krivebot.xyz/permissions)\nError:\n\`\`\`${error || "None."}\`\`\`\n\n[Contact with the bot administration](https://krivebot.xyz/discord)\n[Statuspage](https://status.krivebot.xyz)`)
