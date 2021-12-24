0692427bd693ebfbc18eee072b4214e93160007e
Korrumz2PL <korrumz2@gmail.com>
2021-12-24 22:34:24 +0100

    more comments

--- src/commands/dev/test.js
+++ src/commands/dev/test.js
@@ -1,11 +1,11 @@
-const { MessageEmbed, MessageButton, MessageActionRow } = require("discord.js")
+const { MessageButton, MessageActionRow } = require("discord.js")
 module.exports = {
     name: "test",
     description: "test",
 
     run: async (client, interaction) => {
         let dev = ["817883855310684180"];
-        if (!dev.includes(message.author.id)) return interaction.reply(client.strings.dev.error_permissions);
+        if (!dev.includes(interaction.user.id)) return interaction.reply(client.strings.dev.error_permissions);
 
         const row = new MessageActionRow()
             .addComponents(
--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -95,17 +95,25 @@ module.exports = {
                     .setTimestamp()
                 await client.channels.cache.get(data?.modlogChannel).send({ embeds: [embedModlog2] })
                 break;
+                /*
             case "test-1":
-                interaction.reply({ content: 'Select arg', components: [] });
+                if (interaction.user.id === ) { // 1
+                    interaction.reply({ content: 'Select arg', components: [] });
 
-                const filter = m => m.content
-                const collector = interaction.channel.createMessageCollector({ filter, time: 15000 });
+                    const filter = m => m.content
+                    const collector = interaction.channel.createMessageCollector({ filter, time: 15000 });
 
-                collector.on('collect', m => {
-                    interaction.editReply({ content: `Content: ${m.content}` })
-                    collector.stop();
-                });
+                    collector.on('collect', m => {
+                        interaction.editReply({ content: `Content: ${m.content}` });
+
+                        collector.stop();
+                    });
+                } else {
+                    interaction.reply(`, you can't control this component`); // 2
+                }
                 break;
+                
+                 */
         }
     }
 }
