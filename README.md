13acfb7f8504221fe108084d6c41e4d3e5fcc0cf
Korrumz2PL <korrumz2@gmail.com>
2022-01-03 16:31:48 +0100

    fix

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Launching
+ Launching
 
 ```
 cd src
--- src/commands/bot/links.js
+++ src/commands/bot/links.js
@@ -7,6 +7,6 @@ module.exports = {
         .setDescription('Linki'),
 
     async execute(client, interaction) {
-        client.builder(interaction, "", "**Linki**\n\n[\`Discord\`](https://krivebot.xyz/dicord)\n[\`GitHub\`](https://krivebot.xyz/github)\n[\`Invite\`](https://krivebot.xyz/invite)\n[\`Twitter\`](https://krivebot.xyz/twitter)\n[\`GH Projects\`](https://krivebot.xyz/todo)\n[\`Dokumentacja\`](https://docs.krivebot)\n[\`Status\`](https://status.krivebot.xyz)", `Linki`, `#2f3136`, ``)
+        client.builder(interaction, "", "**Linki**\n\n[\`Discord\`](https://discord.gg/yB3UHCSK4n)\n[\`GitHub\`](https://github.com/Skyndalex/skyndalex)\n[\`Invite\`](https://discord.com/oauth2/authorize?client_id=836529470122622986&permissions=0&scope=bot%20applications.commands)\n[\`GH Projects\`](https://github.com/Skyndalex/skyndalex/projects/4)\n[\`Dokumentacja\`](https://docs.krivebot.xyz)\n[\`Status\`](https://status.krivebot.xyz)", `Linki`, `#2f3136`, ``)
     }
 };
