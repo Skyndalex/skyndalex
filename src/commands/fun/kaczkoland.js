b3665d856786adf28751222702e0325d6664223f
Korrumz2PL <korrumz2@gmail.com>
2022-01-16 23:24:56 +0100

    kaczkoland

--- src/commands/fun/kaczkoland.js
+++ src/commands/fun/kaczkoland.js
@@ -13,7 +13,7 @@ module.exports = {
         { name: "view", description: "View player stats", type: 2, options: [
                 { name: "currentstats", description: "All current server stats", type: 1 },
                 { name: "top", description: "Top ", type: 1, options: [
-                        { name: "mode", description: "Choice top options", required: true, type: 3, choices: [
+                        { name: "mode", description: "Choice top options", required: true, type: "STRING", choices: [
                                 { name: "money", value: "top_money" },
                                 { name: "mined_blocks", value: "top-mined_blocks" },
                                 { name: "placed_blocks", value: "top-placed_blocks" },
@@ -28,24 +28,60 @@ module.exports = {
             ]},
     ],
     run: async (client, interaction) => {
-        if (interaction.options.getSubcommand() === "currentstats") {
-             fetch("https://api.kaczkoland.pl/v2/getSumData")
-                 .then(res => res.json())
-                 .then(async res => {
-                     let embed = new MessageEmbed()
-                         .setTitle("Total Kaczkoland stats")
-                         .setDescription("Data from [api](https://api.kaczkoland.pl/v2/getSumData)")
-                         .addField("Mined blocks", `\`${res.data.minedBlocks}\``, true)
-                         .addField("Placed blocks", `\`${res.data.placedBlocks}\``, true)
-                         .addField(`Crafted items`, `\`${res.data.craftedItems}\``, true)
-                         .addField(`All deaths`, `\`${res.data.allDeaths}\``, true)
-                         .addField(`Killed mobs`, `\`${res.data.killedMobs}\``, true)
-                         .addField(`Mined diamonds`, `\`${res.data.minedDiamonds}\``, true)
-                         .addField(`Total users`, `\`${res.data.totalUsers}\``, true)
-                         .addField(`Total money`, `\`${res.data.money}\``, true)
-                         .setColor("GREEN")
-                     await interaction.reply({ embeds: [embed] })
-                 })
+        switch (interaction.options.getSubcommand()) {
+            case "currentstats":
+                fetch ("https://api.kaczkoland.pl/v2/getSumData")
+                    .then(res => res.json())
+                    .then(async res => {
+                        let embed = new MessageEmbed()
+                            .setTitle("Total Kaczkoland stats")
+                            .setDescription("Data from [\`api\`](https://api.kaczkoland.pl/v2/getSumData).")
+                            .addField("Mined blocks", `\`${res.data.minedBlocks}\``, true)
+                            .addField("Placed blocks", `\`${res.data.placedBlocks}\``, true)
+                            .addField(`Crafted items`, `\`${res.data.craftedItems}\``, true)
+                            .addField(`All deaths`, `\`${res.data.allDeaths}\``, true)
+                            .addField(`Killed mobs`, `\`${res.data.killedMobs}\``, true)
+                            .addField(`Mined diamonds`, `\`${res.data.minedDiamonds}\``, true)
+                            .addField(`Total users (old lobby stats, before the start of survival)`, `\`${res.data.totalUsers}\``, true)
+                            .addField(`Total money`, `\`${res.data.money}$\``, true)
+                            .setColor("GREEN")
+                        await interaction.reply({ embeds: [embed] })
+                    })
+                break;
+            case "top":
+                switch (interaction.options.getString("mode")) {
+                    case "top-mined_blocks":
+                        fetch ("https://api.kaczkoland.pl/v2/getTopPlayers")
+                            .then(res => res.json())
+                            .then(async res => {
+                                let data = [];
+                                for (let i in res.data.minedBlocks) {
+                                    data.push(res.data.minedBlocks[i].player.name);
+                                };
+
+                                let embed = new MessageEmbed()
+                                    .setTitle(`Mined blocks - Top (1 - 5)`)
+                                    .setDescription(`\`\`\`${data.join(",\n").toString()}\`\`\``)
+                                    .setColor("DARK_BUT_NOT_BLACK")
+                                await interaction.reply({ embeds: [embed] })
+                            })
+                    case "top_money":
+                        fetch ("https://api.kaczkoland.pl/v2/getTopPlayers")
+                            .then(res => res.json())
+                            .then(async res => {
+                                let data = [];
+                                for (let i in res.data.money) {
+                                    data.push(res.data.money[i].player.name);
+                                };
+
+                                let embed = new MessageEmbed()
+                                    .setTitle(`Money - Top (1 - 5)`)
+                                    .setDescription(`\`\`\`${data.join(`,\n`)}\`\`\``)
+                                    .setColor("DARK_BUT_NOT_BLACK")
+                                await interaction.reply({ embeds: [embed] })
+                            })
+                        break;
+                }
         }
     }
 };
