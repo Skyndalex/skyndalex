b10bfcc3cd30bad9913c0e86dd471c6f6a98964a
Korrumz2PL <korrumz2@gmail.com>
2022-01-22 11:27:26 +0100

    interaction.commandName

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -15,13 +15,13 @@ module.exports = {
 
             if (cmd) cmd.run(client, interaction).catch(error => {
                 let errorEmbedChannel = new MessageEmbed()
-                    .setDescription(`Server ID: ${interaction.guild.id} (${interaction.guild.name})\nError:\n\`\`\`${error || "None"}\`\`\``)
+                    .setDescription(`Server ID: ${interaction.guild.id} (${interaction.guild.name})\nCommand name: ${interaction.commandName}\nError:\n\`\`\`${error || "None"}\`\`\``)
                     .setColor("DARK_BUT_NOT_BLACK")
                     .setTimestamp()
                 if (error) client.channels.cache.get("914250038744604672").send({embeds: [errorEmbedChannel]})
 
                 let errorEmbed = new MessageEmbed()
-                    .setDescription(`An error has occurred! View [Documentation](https://docs.krivebot.xyz)\nOr [Required bot permissions](https://docs.krivebot.xyz/permissions)\nError:\n\`\`\`${error || "None."}\`\`\`\n\n[Contact with the bot administration](https://krivebot.xyz/discord)\n[Statuspage](https://status.krivebot.xyz)`)
+                    .setDescription(`An error has occurred!\nCommand name: ${interaction.commandName}\n\nView [Documentation](https://docs.krivebot.xyz)\nOr [Required bot permissions](https://docs.krivebot.xyz/permissions)\nError:\n\`\`\`${error || "None."}\`\`\`\n\n[Contact with the bot administration](https://krivebot.xyz/discord)\n[Statuspage](https://status.krivebot.xyz)`)
                     .setColor("DARK_BUT_NOT_BLACK")
                     .setTimestamp()
                 interaction.reply({embeds: [errorEmbed]})
