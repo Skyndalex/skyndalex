7ba8c3d9bc7af4f36bdbd591cd8a9439d0216fa6
entity <55253305+Korrumz2PL@users.noreply.github.com>
2022-04-18 23:18:57 +0200

    add strings json file

--- src/base.js
+++ src/base.js
@@ -1,12 +1,14 @@
 const { Client, MessageEmbed } = require('discord.js');
 const r = require('rethinkdb');
-
+const strings = require('./utils/strings.json');
 class Base extends Client {
     constructor(clientOptions) {
         super(clientOptions);
 
         this.version = 'v4.6';
         this.site = 'https://krivebot.xyz';
+        this.strings = strings;
+
     }
 }
 module.exports = Base;
