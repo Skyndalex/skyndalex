785b3b09621c58b48110e7c22494dc11ace6a1ad
entity <55253305+Korrumz2PL@users.noreply.github.com>
2022-04-18 18:39:54 +0200

    New site

---
+++ src/sites/home/main.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+
+exports.run = (client) => {
+    app.use(express.static(__dirname + '/public'));
+
+    let options = {
+        root: path.join(__dirname, 'public'),
+        dotfiles: 'deny',
+        headers: {
+            'x-timestamp': Date.now(),
+            'x-sent': true
+        }
+    }
+    app.get('/', function (req, res) {
+        res.sendFile('index.html', options)
+    });
+
+    app.listen(3344, () => console.log(`${pc.yellow('[WEBSITE]')} ${pc.green("Connected")}`));
+}
