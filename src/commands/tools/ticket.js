7bb626ca1b067520865c46022cd3309d8a4745c7
entity <55253305+Korrumz2PL@users.noreply.github.com>
2022-04-19 12:35:21 +0200

    remove unused code

--- src/commands/tools/ticket.js
+++ src/commands/tools/ticket.js
@@ -1,8 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName("tickets")
-        .setDescription("Tickets menu")
         .addSubcommand(subcommand =>
             subcommand
                 .setName("enable")
