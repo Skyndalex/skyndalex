d2a0623f5a2d656d081d6533e552e0fafe1f2493
entity <55253305+Korrumz2PL@users.noreply.github.com>
2022-04-20 18:13:02 +0200

    sus command

---
+++ src/slashCommands/dev/test.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed, Message, MessageActionRow, MessageButton, Modal, MessageSelectMenu} = require("discord.js")
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("test")
+        .setDescription("sus command"),
+    async execute(client, interaction) {
+
+    }
+}
