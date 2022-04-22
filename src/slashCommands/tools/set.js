c02af15f3fbe8d2868435e1059dcd32ea0cbe2f2
entity <55253305+Korrumz2PL@users.noreply.github.com>
2022-04-22 22:09:39 +0200

    Remove comments

--- src/slashCommands/tools/set.js
+++ src/slashCommands/tools/set.js
@@ -1,27 +1,4 @@
-/*
-module.exports = {
-    name: "set",
-    description: "Bot settings. Data should be completed one by one from the list of options.",
-    options: [
-        { name: "channels", description: "Data should be completed one by one from the list of options", type: 1, options: [
-                { type: "CHANNEL", name: "broadcast-channel", description: "Broadcast channel" },
-                { type: "CHANNEL", name: "suggestions-channel", description: "Suggestions channel" },
-                { type: "CHANNEL", name: "complaints-channel", description: "Complaints channel" },
-                { type: "CHANNEL", name: "images-channel", description: "Images channel" },
-                { type: "CHANNEL", name: "welcome-channel", description: "Welcome channel" },
-                { type: "CHANNEL", name: "goodbye-channel", description: "Goodbye channel" },
-                { type: "CHANNEL", name: "applications-channel", description: "Applications channel" },
-                { type: "CHANNEL", name: "vote-channel", description: "Voting channel" },
-                { type: "CHANNEL", name: "mod-log", description: "Mod log channel " },
-            ]},
-        { name: "roles", description: "Data should be completed one by one from the list of options", type: 1, options: [
-                { type: "ROLE", name: "moderator-role", description: "Server moderator role [Required for tickets]" },
-                { type: "ROLE", name: "muted-role", description: "The role of the muted user" },
-                { type: "ROLE", name: "user-role", description: "User role [Required for lock, unlock commands]" },
-                { type: "ROLE", name: "auto-role", description: "If a user enters the server, they will automatically get the role set." },
-            ]}
-    ],
- */
+
 const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
