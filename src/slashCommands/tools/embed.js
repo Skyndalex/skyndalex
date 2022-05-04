ad4fb0b387d03c9710844ab6ba0b8f8450d82f49
entity <55253305+Korrumz2PL@users.noreply.github.com>
2022-05-04 14:16:24 +0200

    fix: embed color (default OR)

--- src/slashCommands/tools/embed.js
+++ src/slashCommands/tools/embed.js
@@ -47,7 +47,7 @@ module.exports = {
         let embed = new MessageEmbed()
         if (submitInteraction.fields.getTextInputValue("title")) embed.setTitle(submitInteraction.fields.getTextInputValue("title"))
         if (submitInteraction.fields.getTextInputValue("desc")) embed.setDescription(submitInteraction.fields.getTextInputValue("desc"))
-        if (submitInteraction.fields.getTextInputValue("color")) embed.setColor(submitInteraction.fields.getTextInputValue("color"))
+        if (submitInteraction.fields.getTextInputValue("color")) embed.setColor(submitInteraction.fields.getTextInputValue("color" || "DEFAULT"))
         if (submitInteraction.fields.getTextInputValue("footer")) embed.setFooter({ text: submitInteraction.fields.getTextInputValue("footer")})
         if (submitInteraction.fields.getTextInputValue("author")) embed.setAuthor({ name: submitInteraction.fields.getTextInputValue("author")})
 
