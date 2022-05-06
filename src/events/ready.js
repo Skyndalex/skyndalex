d244908962654148b6637ca54b86a6836c32539b
entity <55253305+Korrumz2PL@users.noreply.github.com>
2022-05-06 21:38:22 +0200

    change folder

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 module.exports = (client, interaction) => {
     client.user.setPresence({
-        activities: [{name: 'HACKED BY POLIGON'}],
+        activities: [{name: 'HACKED BY Cyber#4993'}],
     });
 
     console.log(pc.green(`${pc.yellow('[DISCORD CLIENT]')} Ready`));
