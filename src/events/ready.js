49df0a53b184aefb5387962f23c2f728bf94c64a
entity <55253305+Korrumz2PL@users.noreply.github.com>
2022-05-10 23:45:59 +0200

    change client status information

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
-module.exports = (client, interaction) => {
+module.exports = (client) => {
     client.user.setPresence({
-        activities: [{name: 'HACKED BY MATSTEF'}],
+        activities: [{name: '[⚠️] Global commands are disabled!\nBot testing available only on support: https://discord.gg/RkUYDx5bhM'}],
     });
 
     console.log(pc.green(`${pc.yellow('[DISCORD CLIENT]')} Ready`));
