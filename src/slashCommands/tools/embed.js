77cb1082c1d6927d5d019fbe12123e886213d5cb
entity <55253305+Korrumz2PL@users.noreply.github.com>
2022-05-22 22:48:15 +0200

    add String()

--- src/slashCommands/tools/embed.js
+++
@@ -1,9 +0,0 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("embed")
-        .setDescription("Embed builder"),
-    async execute(client, interaction) {
-        await require("../../modals/embeds/embed").run(client, interaction)
-    }
-}
