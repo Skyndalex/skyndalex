76b1a12f0e4f364359cef56f0b1e70b2213efdcb
entity <55253305+Korrumz2PL@users.noreply.github.com>
2022-05-26 22:06:04 +0200

    fix: settings (syf)

--- src/slashCommands/tools/set.js
+++ src/slashCommands/tools/set.js
@@ -14,7 +14,6 @@ module.exports = {
                 .addChannelOption(option => option.setName("imageschannel").setDescription("Images channel"))
                 .addChannelOption(option => option.setName("welcomechannel").setDescription("Welcome channel"))
                 .addChannelOption(option => option.setName("goodbyechannel").setDescription("Goodbye channel"))
-                .addChannelOption(option => option.setName("applicationschannel").setDescription("Applications channel"))
                 .addChannelOption(option => option.setName("modlog").setDescription("Modlog channel")),
         )
         .addSubcommand(subcommand =>
@@ -27,13 +26,18 @@ module.exports = {
                 .addRoleOption(option => option.setName("auto").setDescription("Auto role"))
         ),
     async execute(client, interaction) {
-        /*
         const data = interaction.options._hoistedOptions.map(x => x.value)
         console.log(data)
 
-        if (data[0]) r.table("settings").insert({ id: interaction.guild.id, broadcastChannel: data[0]}, { conflict: "update" }).run(client.con)
-        if (data[1]) r.table("settings").insert({ id: interaction.guild.id, suggestionsChannel: data[1]}, { conflict: "update" }).run(client.con)
-         */
+        // There were problems with the switch(data) [...]. Definitely better to use ifs. I know it doesn't look interesting.
+        //
+        if (data[0]) await r.table("settings").insert({ id: interaction.guild.id, broadcastChannel: data[0]}, { conflict: "update" }).run(client.con)
+        if (data[1]) await r.table("settings").insert({ id: interaction.guild.id, suggestionsChannel: data[1]}, { conflict: "update" }).run(client.con);
+        if (data[2]) await r.table("settings").insert({ id: interaction.guild.id, complaintsChannel: data[2]}, { conflict: "update" }).run(client.con);
+        if (data[3]) await r.table("settings").insert({ id: interaction.guild.id, imagesChannel: data[3]}, { conflict: "update" }).run(client.con);
+        if (data[4]) await r.table("settings").insert({ id: interaction.guild.id, welcomeChannel: data[4]}, { conflict: "update" }).run(client.con);
+        if (data[5]) await r.table("settings").insert({ id: interaction.guild.id, goodbyeChannel: data[5]}, { conflict: "update" }).run(client.con);
+        if (data[6]) await r.table("settings").insert({ id: interaction.guild.id, modLogChannel: data[6]}, { conflict: "update" }).run(client.con);
 
     }
 }
