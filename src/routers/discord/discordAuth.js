0606ec74ec434c701387e77fac24ec793813d3a7
entity <55253305+Korrumz2PL@users.noreply.github.com>
2022-06-17 22:04:57 +0200

    Fix skill issue!!!

--- src/routers/discord/discordAuth.js
+++ src/routers/discord/discordAuth.js
@@ -1,68 +1,69 @@
 const router = require('express').Router()
-const express = require('express')
 const { fetch } = require("undici");
-const { secret, clientID } = require("../../config.json").discord
-    router.get('/discordData', async (req, res) => {
-        if (!req.session.user) return res.redirect('https://discord.com/api/oauth2/authorize?client_id=829812129074774086&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fdiscord%2Fcallback&response_type=code&scope=identify%20guilds')
+const { secret, clientID, callbackURL } = require("../../config.json").discord
 
-        res.send(req.session.user)
-        console.log(req.session.user)
+router.get('/discordData', async (req, res) => {
+    if (!req.session.user) return res.redirect(`https://discord.com/api/oauth2/authorize?client_id=829812129074774086&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fdiscord%2Fcallback&response_type=code&scope=identify%20guilds`)
 
-        await r.table("trello").insert({ uid: req.session.user.id }, { conflict: "update" }).run(req.client.con)
+    res.send(req.session.user)
+    console.log(req.session.user)
+
+    await r.table("trello").insert({ uid: req.session.user.id }, { conflict: "update" }).run(req.client.con)
+})
+
+router.get('/callback', async (req, res) => {
+    if (!req.query.code) return res.send({ message: 'Query code is invalid!' })
+
+    const params = new URLSearchParams()
+
+    params.set('grant_type', 'authorization_code')
+    params.set('code', req.query.code)
+    params.set('redirect_uri', "http://localhost:3000/discord/callback")
+
+    let response = await fetch('https://discord.com/api/oauth2/token', {
+        method: 'POST',
+        body: params.toString(),
+        headers: {
+            authorization: `Basic ${btoa(`${clientID}:${secret}`)}`,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
     })
 
-    router.get('/callback', async (req, res) => {
-        if (!req.query.code) return res.send({ message: 'Query code is invalid!' })
+    const tokens = await response.json()
 
-        const params = new URLSearchParams()
+    const userData = {
+        info: null,
+        guilds: null
+    }
 
-        params.set('grant_type', 'authorization_code')
-        params.set('code', req.query.code)
-        params.set('redirect_uri', `http://localhost:3000/discord/callback`)
-
-        let response = await fetch('https://discord.com/api/oauth2/token', {
-            method: 'POST',
-            body: params.toString(),
+    if (!userData.info) {
+        response = await fetch('https://discord.com/api/users/@me', {
+            method: 'GET',
             headers: {
-                authorization: `Basic ${btoa(`${clientID}:${secret}`)}`,
-                'Content-Type': 'application/x-www-form-urlencoded',
+                authorization: `Bearer ${tokens.access_token}`
             }
         })
 
-        const tokens = await response.json()
+        userData.info = await response.json()
+    }
 
-        const userData = {
-            info: null,
-            guilds: null,
-        }
-// h
-        if (!userData.info) {
-            response = await fetch('https://discord.com/api/users/@me', {
-                method: 'GET',
-                headers: {
-                    authorization: `Bearer ${tokens.access_token}`,
-                }
-            })
+    if (!userData.guilds) {
+        response = await fetch('https://discord.com/api/users/@me/guilds', {
+            method: 'GET',
+            headers: {
+                authorization: `Bearer ${tokens.access_token}`
+            }
+        })
 
-            userData.info = await response.json()
-        }
+        userData.guilds = await response.json()
+    }
 
-        if (!userData.guilds) {
-            response = await fetch('https://discord.com/api/users/@me/guilds', {
-                method: 'GET',
-                headers: {
-                    authorization: `Bearer ${tokens.access_token}`,
-                }
-            })
+    const guilds = [];
+    for (const guildPos in userData.guilds) guilds.push(userData.guilds[guildPos]);
 
-            userData.guilds = await response.json()
-        }
+    req.session.user = { ...userData.info, ... { guilds } }
 
-        const guilds = [];
-        for (const guildPos in userData.guilds) guilds.push(userData.guilds[guildPos]);
+    res.redirect('/discord/discordData')
+})
 
-        req.session.user = { ...userData.info, ... { guilds } }
-
-        res.redirect('/discord/discordData')
-    })
 module.exports = router
