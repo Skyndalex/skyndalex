5a6009971306433910196043f20a99e0281b4ab2
entity <55253305+Korrumz2PL@users.noreply.github.com>
2022-06-21 22:37:44 +0200

    trello: comments

--- src/interactions/buttons/trello.js
+++ src/interactions/buttons/trello.js
@@ -29,9 +29,9 @@ exports.run = async (client, interaction) => {
             const cardDesc = addCardString.fields[0].value
             const listID = addCardString.fields[2].value
 
-            const cardAdd = await fetch(`https://api.trello.com/1/cards?idList=${listID}&key=${db.key}&token=${db.token}&name=${cardName}&desc=${cardDesc}`, {
-                method: "POST"
-            });
+            const cardAdd = await fetch(`https://api.trello.com/1/cards?idList=${listID}&key=${db.key}&token=${db.token}&name=${cardName}&desc=${cardDesc}`,
+                { method: "POST" }
+            );
             const cardAddedData = await cardAdd.json()
 
             // console.log(json)
@@ -68,32 +68,50 @@ exports.run = async (client, interaction) => {
             let organizationID = boardAddString.fields[1].value;
             let description = boardAddString.fields[2].value;
 
-            const addBoard = await fetch(`https://api.trello.com/1/boards/?name=${bName}&key=${db.key}&token=${db.token}&idOrganization=${organizationID}&desc=${description}`, {
-                method: "POST"
-            })
+            const addBoard = await fetch(`https://api.trello.com/1/boards/?name=${bName}&key=${db.key}&token=${db.token}&idOrganization=${organizationID}&desc=${description}`,
+                { method: "POST" }
+            )
             const addedBoard = addBoard.json()
            // console.log(addedBoard)
 
             await interaction.reply("OK")
+
             break;
         case "add_checklist_confirm":
-            if (interaction.message.interaction.user.id !== interaction.user.id) return interaction.reply({ content: "It's not your button!", ephemeral: true })
+            if (interaction.message.user.id !== interaction.user.id) return interaction.reply({ content: "It's not your button!", ephemeral: true })
 
             let addChecklistString = interaction.message.embeds[0];
 
             let cardID2 = addChecklistString.fields[0].value;
             let checklistName = addChecklistString.fields[1].value;
 
-            const addChecklist = await fetch(`https://api.trello.com/1/checklists?idCard=${cardID2}&key=${db.key}&token=${db.token}&name=${checklistName}`, {
-                method: "POST"
-            })
+            const addChecklist = await fetch(`https://api.trello.com/1/checklists?idCard=${cardID2}&key=${db.key}&token=${db.token}&name=${checklistName}`,
+                { method: "POST" }
+            )
+
             const addedChecklist = await addChecklist.json()
 
             let addedChecklistSuccess = new MessageEmbed()
                 .setDescription(`\`[${addedChecklist.name}]\` successfully added checklist`)
                 .setColor("DARK_BUT_NOT_BLACK")
             interaction.reply({ embeds: [addedChecklistSuccess] })
+            break;
+        case "add_comment_confirm":
+            let addCommentString = interaction.message.embeds[0];
 
+            let cardID3 = addCommentString.fields[0].value;
+            let commentDescription = addCommentString.fields[1].value;
+
+            const addComment = await fetch(`https://api.trello.com/1/cards/${cardID3}/actions/comments?text=${commentDescription}&key=${db.key}&token=${db.token}`,
+                { method: "POST" }
+            )
+            const addedComment = await addComment.json()
+            console.log(addedComment)
+
+            let addedCommentSuccess = new MessageEmbed()
+                .setDescription(`\`[${addedComment.data.text}]\` successfully added comment to card **${addedComment.data.card.name}**`)
+                .setColor("DARK_BUT_NOT_BLACK")
+            interaction.reply({ embeds: [addedCommentSuccess] })
             break;
 
     }
--- src/slashCommands/tools/trello.js
+++ src/slashCommands/tools/trello.js
@@ -264,6 +264,55 @@ module.exports = {
                             await interaction.reply({ embeds: [messageConfirmEmbed3], components: [addChecklistConfirm] })
                         })
                         break;
+                    case "add_comment_to_card":
+                        const addCommentToCardModal = new Modal()
+                            .setTitle("Add comment")
+                            .setCustomId("add_comment_to_card_modal")
+
+                        const addCommentToCard_CardID = new TextInputComponent()
+                            .setStyle("SHORT")
+                            .setRequired(true)
+                            .setPlaceholder("Card ID (get card ids: /trello options)")
+                            .setMaxLength(100)
+                            .setCustomId("add_comment_card_id_component")
+                            .setLabel("Card ID")
+
+                        const addCommentToCard_Text = new TextInputComponent()
+                            .setStyle("PARAGRAPH")
+                            .setRequired(true)
+                            .setPlaceholder("Comment")
+                            .setMaxLength(1000)
+                            .setCustomId("add_comment_description_component")
+                            .setLabel("Comment description")
+
+                        const addCommentCardIdRow = new MessageActionRow().addComponents(addCommentToCard_CardID)
+                        const addCommentDescriptionRow = new MessageActionRow().addComponents(addCommentToCard_Text)
+
+                        addCommentToCardModal.addComponents(addCommentCardIdRow, addCommentDescriptionRow)
+                        await interaction.showModal(addCommentToCardModal)
+
+                        const addCommentFilter = (interaction) => interaction.customId === "add_comment_to_card_modal";
+                        await interaction.awaitModalSubmit({ addCommentFilter, time: 15000 }).then(async interaction => {
+                            let cardID = interaction.fields.getTextInputValue("add_comment_card_id_component")
+                            let commentDescription = interaction.fields.getTextInputValue("add_comment_description_component")
+
+                            let addCommentConfirm = new MessageActionRow()
+                                .addComponents(
+                                    new MessageButton()
+                                        .setCustomId("add_comment_confirm")
+                                        .setStyle("SUCCESS")
+                                        .setLabel("Confirm")
+                                )
+
+                            let messageConfirmEmbed4 = new MessageEmbed()
+                                .setTitle("Are you sure?")
+                                .setDescription("You provided these values:")
+                                .addField(`Card ID`, `${cardID}`)
+                                .addField(`Comment description`, `${commentDescription || "None"}`)
+                                .setColor("BLUE")
+                            await interaction.reply({ embeds: [messageConfirmEmbed4], components: [addCommentConfirm] })
+                        })
+                        break;
                 }
                 const get = await interaction.options.getString("get");
 
